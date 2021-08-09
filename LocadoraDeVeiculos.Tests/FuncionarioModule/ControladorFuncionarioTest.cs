188752e060825662983cae8fb50a66904e04a969
Pedro Dircksen <dircksenpedro@gmail.com>
2021-08-09 16:59:37 -0300

    Implementado o teste de editar um funcionario

--- LocadoraDeVeiculos.Tests/FuncionarioModule/ControladorFuncionarioTest.cs
+++ LocadoraDeVeiculos.Tests/FuncionarioModule/ControladorFuncionarioTest.cs
@@ -29,5 +29,18 @@ namespace LocadoraDeVeiculos.Tests.FuncionarioModule
             var funcionarioEncontrado = controlador.SelecionarPorId(novoFuncionario.Id);
             funcionarioEncontrado.Should().Be(novoFuncionario);
         }
+
+        [TestMethod]
+        public void DeveAtualizarFuncionario()
+        {
+            var funcionario = new Funcionario("Pedro", new DateTime(2020, 01, 01), 1000, "password");
+            controlador.InserirNovo(funcionario);
+            var novoFuncionario = new Funcionario("João", new DateTime(2021, 02, 02), 5000, "p4ssw0rd");
+
+            controlador.Editar(funcionario.Id, novoFuncionario);
+
+            var funcionarioAtualizado = controlador.SelecionarPorId(funcionario.Id);
+            funcionarioAtualizado.Should().Be(novoFuncionario);
+        }
     }
 }
