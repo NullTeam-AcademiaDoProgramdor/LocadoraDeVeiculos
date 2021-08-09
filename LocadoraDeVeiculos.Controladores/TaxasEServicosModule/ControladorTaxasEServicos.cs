070d29ab213990d901885dc5502c06b383b6e445
Diconcili <eduardodiconcilicosta@yahoo.com.br>
2021-08-09 14:59:33 -0300

    Implementação do método InserirNovo no Controlador

--- LocadoraDeVeiculos.Controladores/TaxasEServicosModule/ControladorTaxasEServicos.cs
+++ LocadoraDeVeiculos.Controladores/TaxasEServicosModule/ControladorTaxasEServicos.cs
@@ -3,10 +3,84 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LocadoraDeVeiculos.Controladores.Shared;
+using LocadoraDeVeiculos.Dominio.TaxasEServicosModule;
 
 namespace LocadoraDeVeiculos.Controladores.TaxasEServicosModule
 {
-    class ControladorTaxasEServicos
+    class ControladorTaxasEServicos : Controlador<TaxasEServicos>
     {
+
+        #region Queries
+        private const string sqlInserirTaxaEServico =
+            @"INSERT INTO [TAXAESERVICO]
+                (
+                    [EHFIXO],       
+                    [PRECO],             
+                    [NOME]     
+                )
+            VALUES
+                (
+                   @EHFIXO
+                   @PRECO
+                   @NOME
+                 )";
+
+        private const string sqlEditarTaxaEServico =
+            @" UPDATE [TAXAESERVICO]
+                SET 
+                    [EHFIXO] = @EHFIXO, 
+                    [PRECO] = @PRECO, 
+                    [NOME] =  @NOME                    
+                WHERE [ID] = @ID";
+
+        private const string sqlExcluirTaxaEServico =
+            @"DELETE FROM [TAXAESERVICO] 
+                WHERE [ID] = @ID;";
+
+        private const string sqlSelecionarTodasTaxasEServicos =
+            @"SELECT 
+                [EHFIXO],       
+                [PRECO],             
+                [NOME]
+              FROM
+                [TAXAESERVICO]";
+
+        private const string sqlSelecionarTaxaPorNome =
+            @"SELECT 
+                [EHFIXO],       
+                [PRECO],             
+                [NOME]
+             FROM
+                [TAXAESERVICO]
+             WHERE 
+                [NOME] = @NOME";
+        #endregion
+
+        public override string InserirNovo(TaxasEServicos taxaOuServico)
+        {
+            string resultadoValidacao = taxaOuServico.Validar();
+
+            if (resultadoValidacao == "ESTA_VALIDO")
+            {
+                taxaOuServico.id = Db.Insert(sqlInserirTaxaEServico, ObtemParametrosTaxaEServico(taxaOuServico));
+            }
+
+            return resultadoValidacao;
+        }
+
+
+
+        private Dictionary<string, object> ObtemParametrosTaxaEServico(TaxasEServicos taxaOuServico)
+        {
+            var parametros = new Dictionary<string, object>();
+
+            parametros.Add("ID", taxaOuServico.id);
+            parametros.Add("NOME", taxaOuServico.Nome);
+            parametros.Add("PRECO", taxaOuServico.Preco);
+            parametros.Add("DATACRIACAO", taxaOuServico.EhFixo);           
+
+            return parametros;
+        }
     }
 }
--- LocadoraDeVeiculos.Dominio/TaxasEServicosModule/TaxasEServicos.cs
+++ LocadoraDeVeiculos.Dominio/TaxasEServicosModule/TaxasEServicos.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LocadoraDeVeiculos.Dominio.Shared;
 
 namespace LocadoraDeVeiculos.Dominio.TaxasEServicosModule
 {
-    class TaxasEServicos
+    public class TaxasEServicos : EntidadeBase
     {
         string nome;
         double preco;
@@ -17,11 +18,52 @@ namespace LocadoraDeVeiculos.Dominio.TaxasEServicosModule
             this.Nome = nome;
             this.Preco = preco;
             this.EhFixo = ehFixo;
-        }
+        }               
 
         public string Nome { get => nome; set => nome = value; }
         public double Preco { get => preco; set => preco = value; }
-        public bool EhFixo { get => ehFixo; set => ehFixo = value; }
+        public bool EhFixo { get => ehFixo; set => ehFixo = value; }        
+
+        public override bool Equals(object obj)
+        {
+            return obj is TaxasEServicos servicos &&
+                   id == servicos.id &&
+                   nome == servicos.nome &&
+                   preco == servicos.preco &&
+                   ehFixo == servicos.ehFixo &&
+                   Nome == servicos.Nome &&
+                   Preco == servicos.Preco &&
+                   EhFixo == servicos.EhFixo;
+        }
+
+        public override string Validar()
+        {
+            string resultadoValidacao = "";
+
+            if(!(Nome is string))            
+                resultadoValidacao += " O campo nome está inválido";            
+
+            if(!(Preco is double))            
+                resultadoValidacao += " O campo preço está inválido";            
+
+            if(!(EhFixo is bool))            
+                resultadoValidacao += " O campo ehFixo está inválido";            
+
+            if(resultadoValidacao == "")            
+                resultadoValidacao = "ESTA_VALIDO";            
+
+            return resultadoValidacao;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 1695060689;
+            hashCode = hashCode * -1521134295 + id.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Nome);
+            hashCode = hashCode * -1521134295 + EqualityComparer<double>.Default.GetHashCode(Preco);
+            hashCode = hashCode * -1521134295 + EqualityComparer<bool>.Default.GetHashCode(EhFixo);            
+            return hashCode;
+        }
 
     }
 }
