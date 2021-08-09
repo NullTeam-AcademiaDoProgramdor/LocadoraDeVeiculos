eefb79a07973873018cffc35e4ae1f14add06e8f
Matheus F Lopes <matheusdefigueiredo@gmail.com>
2021-08-09 17:56:28 -0300

    Criando Classe de Testa para domínio Pessoa Jurídica.

---
+++ LocadoraDeVeiculos.Tests/PessoaJuridicaModule/PessoaJuridicaTest.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using LocadoraDeVeiculos.Dominio.PessoaJuridicaModule;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LocadoraDeVeiculos.Tests.PessoaJuridicaModule
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    [TestCategory("Dominio")]
+    public class PessoaJuridicaTest
+    {
+        [TestMethod]
+        public void DeveCriar_PessoaJuridica()
+        {
+            //action
+            PessoaJuridica pessoaJuridica = new PessoaJuridica("Matheus", "22.000.000/0001-00", "(49)000000000", "Lagi");
+
+            //assert
+            pessoaJuridica.Nome.Should().Be("Matheus");
+            pessoaJuridica.Cnpj.Should().Be("22.000.000/0001-00");
+            pessoaJuridica.Telefone.Should().Be("(49)000000000");
+            pessoaJuridica.Endereco.Should().Be("Lagi");
+        }
+    }
+}
