2c348907f43754e176a9e07bfce44ec8701ef6f7
Erik Borela <erikborella@gmail.com>
2021-08-09 16:53:46 -0300

    Criado teste para editar um GrupoDeAutomovel

--- LocadoraDeVeiculos.Tests/GrupoAutomovelModule/ControladorGupoAutomovelTest.cs
+++ LocadoraDeVeiculos.Tests/GrupoAutomovelModule/ControladorGupoAutomovelTest.cs
@@ -27,7 +27,6 @@ namespace LocadoraDeVeiculos.Tests.GrupoAutomovelModule
             Db.Update("DELETE FROM [GrupoAutomovel]");
         }
 
-
         [TestMethod]
         public void DeveInserir_GrupoAutomovel()
         {
@@ -43,5 +42,32 @@ namespace LocadoraDeVeiculos.Tests.GrupoAutomovelModule
             GrupoAutomovel grupoEncontrado = controlador.SelecionarPorId(novoGrupo.id);
             grupoEncontrado.Should().Be(novoGrupo);
         }
+
+        [TestMethod]
+        public void DeveEditar_UmGrupoAutomovel()
+        {
+            GrupoAutomovel grupo = new GrupoAutomovel(
+                "Economicos",
+                new PlanoDiarioStruct(150, 5),
+                new PlanoKmControladoStruct(100, 15, 100),
+                new PlanoKmLivreStruct(300)
+            );
+            controlador.InserirNovo(grupo);
+
+
+            GrupoAutomovel novoGrupo = new GrupoAutomovel(
+                "SUV",
+                new PlanoDiarioStruct(200, 10),
+                new PlanoKmControladoStruct(150, 20, 150),
+                new PlanoKmLivreStruct(500)
+            );
+
+            controlador.Editar(grupo.id, novoGrupo);
+
+            GrupoAutomovel grupoEncontrado = controlador.SelecionarPorId(grupo.id);
+            grupoEncontrado.Should().Be(novoGrupo);
+        }
+
+       
     }
 }
