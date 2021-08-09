eae6f6f88bbe428ded25fa0f564df82dfd304571
Matheus F Lopes <matheusdefigueiredo@gmail.com>
2021-08-09 16:24:13 -0300

    Implementada a Configuração Tool Box Pessoa Jurídica.

---
+++ LocadoraDeVeiculos.WindowsApp/Features/PessoasJuridicas/ConfiguracaoPessoaJuridicaToolBox.cs
@@ -0,0 +1,43 @@
+﻿using LocadoraDeVeiculos.WindowsApp.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.WindowsApp.Features.PessoasJuridicas
+{
+    public class ConfiguracaoPessoaJuridicaToolBox : IConfiguracaoToolBox
+    {
+        public ConfiguracoesTooltip Tooltip
+        {
+            get
+            {
+                return new ConfiguracoesTooltip()
+                {
+                    TipoCadastro = "Cadastro de Pessoa Jurídica",
+                    ToolTipAdicionar = "Adicionar uma nova Pessoa Jurídica",
+                    ToolTipEditar = "Editar uma Pessoa Jurídica existente",
+                    ToolTipExcluir = "Excluir uma Pessoa Jurídica existente",
+                    ToolTipFiltrar = "Filtrar Pessoas Jurídicas",
+                    ToolTipAgrupar = "Expandir Pessoa Jurídica",
+                    ToolTípDesagrupar = "Esconder Pessoa Jurídica",
+
+                };
+            }
+        }
+
+
+
+        public ConfiguracoesBotoes Botoes
+        {
+            get
+            {
+                return new ConfiguracoesBotoes()
+                {
+                    BtnFiltrar = true,
+                };
+            }
+        }
+    }
+}
