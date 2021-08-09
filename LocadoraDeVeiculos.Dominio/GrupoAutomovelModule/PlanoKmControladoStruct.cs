f73523e3a6212b8431e89c437d751c33ef1d6f05
Erik Borela <erikborella@gmail.com>
2021-08-09 04:23:21 -0300

    Criado struct PlanoKmControlado

---
+++ LocadoraDeVeiculos.Dominio/GrupoAutomovelModule/PlanoKmControladoStruct.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Dominio.GrupoAutomovelModule
+{
+    public struct PlanoKmControladoStruct
+    {
+        public float PrecoDia { get; }
+        public float PrecoKmExtrapolado { get; }
+        public float KmDisponiveis { get; }
+
+        public PlanoKmControladoStruct(float precoDia, float precoKmExtrapolado, float kmDisponiveis)
+        {
+            PrecoDia = precoDia;
+            PrecoKmExtrapolado = precoKmExtrapolado;
+            KmDisponiveis = kmDisponiveis;
+        }
+    }
+}
