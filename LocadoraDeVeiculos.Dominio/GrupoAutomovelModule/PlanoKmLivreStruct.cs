efb8478642e35cfcbcf150350859694396509012
Erik Borela <erikborella@gmail.com>
2021-08-09 04:23:33 -0300

    Criado struct PlanoKmLivre

---
+++ LocadoraDeVeiculos.Dominio/GrupoAutomovelModule/PlanoKmLivreStruct.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Dominio.GrupoAutomovelModule
+{
+    public struct PlanoKmLivreStruct
+    {
+        public float PrecoDia { get; }
+
+        public PlanoKmLivreStruct(float precoDia)
+        {
+            PrecoDia = precoDia;
+        }
+    }
+}
