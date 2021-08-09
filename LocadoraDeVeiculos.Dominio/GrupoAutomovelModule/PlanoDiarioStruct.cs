b6553e8d9f456f9b6c5022f001342c35b1676f33
Erik Borela <erikborella@gmail.com>
2021-08-09 04:23:00 -0300

    Criado struct PlanoDiario

---
+++ LocadoraDeVeiculos.Dominio/GrupoAutomovelModule/PlanoDiarioStruct.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Dominio.GrupoAutomovelModule
+{
+    public struct PlanoDiarioStruct
+    {
+        public float PrecoDia { get; }
+        public float PrecoKm { get; }
+
+        public PlanoDiarioStruct(float precoDia, float precoKm)
+        {
+            PrecoDia = precoDia;
+            PrecoKm = precoKm;
+        }
+    }
+}
