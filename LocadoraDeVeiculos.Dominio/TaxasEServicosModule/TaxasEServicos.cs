1d67bf46ad13559c43ed6f05d683e23ae6d1ef03
Diconcili <eduardodiconcilicosta@yahoo.com.br>
2021-08-09 14:12:58 -0300

    Atributos e construtor adicionados à classe TaxasEServicos

--- LocadoraDeVeiculos.Dominio/TaxasEServicosModule/TaxasEServicos.cs
+++ LocadoraDeVeiculos.Dominio/TaxasEServicosModule/TaxasEServicos.cs
@@ -8,5 +8,20 @@ namespace LocadoraDeVeiculos.Dominio.TaxasEServicosModule
 {
     class TaxasEServicos
     {
+        string nome;
+        double preco;
+        bool ehFixo;
+
+        public TaxasEServicos(string nome, double preco, bool ehFixo)
+        {
+            this.Nome = nome;
+            this.Preco = preco;
+            this.EhFixo = ehFixo;
+        }
+
+        public string Nome { get => nome; set => nome = value; }
+        public double Preco { get => preco; set => preco = value; }
+        public bool EhFixo { get => ehFixo; set => ehFixo = value; }
+
     }
 }
