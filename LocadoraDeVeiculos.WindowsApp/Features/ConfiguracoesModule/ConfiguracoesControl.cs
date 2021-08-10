9e697271857dbcde293d9d08615d50c344a1abfa
Matheus F Lopes <matheusdefigueiredo@gmail.com>
2021-08-10 16:07:08 -0300

    Design User Control Configuracoes.

---
+++ LocadoraDeVeiculos.WindowsApp/Features/ConfiguracoesModule/ConfiguracoesControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LocadoraDeVeiculos.WindowsApp.Features.ConfiguracoesModule
+{
+    public partial class ConfiguracoesControl : UserControl
+    {
+        public ConfiguracoesControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
