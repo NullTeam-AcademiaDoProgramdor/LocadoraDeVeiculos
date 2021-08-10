73c0259fe123c9dc9335521791115549d9ef03e3
Erik Borela <erikborella@gmail.com>
2021-08-10 15:49:01 -0300

    Criado classe de configuração

---
+++ LocadoraDeVeiculos.Configuracoes/Configuracao.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Configuracoes
+{
+    public static class Configuracao
+    {
+        public static double PrecoGasolina { get; set; }
+        public static double PrecoDiesel { get; set; }
+        public static double PrecoAlcool { get; set; }
+
+        public static TimeSpan HoraAbertura { get; set; }
+        public static TimeSpan HoraFechamento { get; set; }
+
+        public static bool AbreNoSabado { get; set; }
+        public static bool AbreNoDomingo { get; set; }
+    }
+}
