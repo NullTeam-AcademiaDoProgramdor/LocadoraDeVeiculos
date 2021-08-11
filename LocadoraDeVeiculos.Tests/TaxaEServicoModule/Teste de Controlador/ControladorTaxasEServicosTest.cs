030340b34c72f657d0cb3ae26c7f1068e53ca8fd
Diconcili <eduardodiconcilicosta@yahoo.com.br>
2021-08-11 13:45:21 -0300

    Implementado teste de selecionar todos no controlador

--- LocadoraDeVeiculos.Tests/TaxaEServicoModule/Teste de Controlador/ControladorTaxasEServicosTest.cs
+++ LocadoraDeVeiculos.Tests/TaxaEServicoModule/Teste de Controlador/ControladorTaxasEServicosTest.cs
@@ -4,6 +4,7 @@ using LocadoraDeVeiculos.Controladores.TaxasEServicosModule;
 using LocadoraDeVeiculos.Dominio.TaxasEServicosModule;
 using LocadoraDeVeiculos.Controladores.Shared;
 using FluentAssertions;
+using System.Collections.Generic;
 
 namespace LocadoraDeVeiculos.Tests.TaxaEServicoModule.Teste_de_Controlador
 {
@@ -45,7 +46,7 @@ namespace LocadoraDeVeiculos.Tests.TaxaEServicoModule.Teste_de_Controlador
             controlador.Editar(novaTaxa.Id, outraTaxa);
             
             TaxaEServico taxaEncontrada = controlador.SelecionarPorId(novaTaxa.Id);
-            taxaEncontrada.Should().Be(novaTaxa);
+            taxaEncontrada.Should().Be(outraTaxa);
         }
 
         [TestMethod]
@@ -91,6 +92,28 @@ namespace LocadoraDeVeiculos.Tests.TaxaEServicoModule.Teste_de_Controlador
             TaxaEServico taxaEncontrada = controlador.SelecionarPorId(novaTaxa.Id);
 
             taxaEncontrada.Should().Be(novaTaxa);
+        }
+
+        [TestMethod]
+        public void DeveSelecionar_Todas_Taxas()
+        {
+            TaxaEServico novaTaxa = new TaxaEServico("a", 10, true);
+            controlador.InserirNovo(novaTaxa);
+
+            TaxaEServico outraTaxa = new TaxaEServico("b", 20, true);
+            controlador.InserirNovo(outraTaxa);
+
+            TaxaEServico maisUmaTaxa = new TaxaEServico("C", 30, false);
+            controlador.InserirNovo(maisUmaTaxa);
+
+            List<TaxaEServico> taxasParaVerificar = new List<TaxaEServico>();
+            taxasParaVerificar.Add(novaTaxa);
+            taxasParaVerificar.Add(outraTaxa);
+            taxasParaVerificar.Add(maisUmaTaxa);
+
+            List<TaxaEServico> taxasSelecionadas = controlador.SelecionarTodos();   
+
+            Assert.AreEqual(taxasSelecionadas.ToString(), taxasParaVerificar.ToString());
         }
     }
 }
