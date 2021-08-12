5b0fbce4e6711a3509d7665b447a7b3de59c7f8a
Matheus F Lopes <matheusdefigueiredo@gmail.com>
2021-08-12 16:45:37 -0300

    Implemantados os métodos Equals e GetHashCode para Pessoa Física (domínio).

--- LocadoraDeVeiculos.Dominio/PessoaFisicaModule/PessoaFisica.cs
+++ LocadoraDeVeiculos.Dominio/PessoaFisicaModule/PessoaFisica.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace LocadoraDeVeiculos.Dominio.PessoaFisicaModule
 {
-    public class PessoaFisica
+    public class PessoaFisica : IEquatable<PessoaFisica>
     {
         public PessoaFisica(string nome, string cPF, string rG, string cNH,
             string telefone, string endereco, PessoaJuridica pessoaJuridica = null)
@@ -31,5 +31,35 @@ namespace LocadoraDeVeiculos.Dominio.PessoaFisicaModule
         public string Telefone { get; }
         public string Endereco { get; }
         public PessoaJuridica PessoaJuridica { get; }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as PessoaFisica);
+        }
+
+        public bool Equals(PessoaFisica other)
+        {
+            return other != null &&
+                   Nome == other.Nome &&
+                   CPF == other.CPF &&
+                   RG == other.RG &&
+                   CNH == other.CNH &&
+                   Telefone == other.Telefone &&
+                   Endereco == other.Endereco &&
+                   EqualityComparer<PessoaJuridica>.Default.Equals(PessoaJuridica, other.PessoaJuridica);
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 1083796582;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Nome);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(CPF);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(RG);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(CNH);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Telefone);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Endereco);
+            hashCode = hashCode * -1521134295 + EqualityComparer<PessoaJuridica>.Default.GetHashCode(PessoaJuridica);
+            return hashCode;
+        }
     }
 }
