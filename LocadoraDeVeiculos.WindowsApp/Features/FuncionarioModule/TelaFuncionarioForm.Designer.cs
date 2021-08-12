6a79bd1c18efce6f8577cd20a0b64dd298986a4c
Pedro Dircksen <dircksenpedro@gmail.com>
2021-08-12 14:59:39 -0300

    Implementado alterações de design na tela de cadastro de funcionários

--- LocadoraDeVeiculos.WindowsApp/Features/FuncionarioModule/TelaFuncionarioForm.Designer.cs
+++ LocadoraDeVeiculos.WindowsApp/Features/FuncionarioModule/TelaFuncionarioForm.Designer.cs
@@ -41,61 +41,62 @@ namespace LocadoraDeVeiculos.WindowsApp.Features.FuncionarioModule
             this.txtId = new System.Windows.Forms.TextBox();
             this.btnGravar = new System.Windows.Forms.Button();
             this.btnCancelar = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(86, 47);
+            this.label1.Location = new System.Drawing.Point(76, 45);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(38, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Nome";
+            this.label1.Text = "Nome:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(29, 73);
+            this.label2.Location = new System.Drawing.Point(19, 71);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.Size = new System.Drawing.Size(95, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Data de admissão";
+            this.label2.Text = "Data de admissão:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(82, 99);
+            this.label3.Location = new System.Drawing.Point(72, 97);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.Size = new System.Drawing.Size(42, 13);
             this.label3.TabIndex = 2;
-            this.label3.Text = "Salário";
+            this.label3.Text = "Salário:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(83, 125);
+            this.label4.Location = new System.Drawing.Point(73, 123);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.Size = new System.Drawing.Size(41, 13);
             this.label4.TabIndex = 3;
-            this.label4.Text = "Senha";
+            this.label4.Text = "Senha:";
             // 
             // txtNome
             // 
-            this.txtNome.Location = new System.Drawing.Point(127, 44);
+            this.txtNome.Location = new System.Drawing.Point(117, 42);
             this.txtNome.Name = "txtNome";
             this.txtNome.Size = new System.Drawing.Size(177, 20);
             this.txtNome.TabIndex = 4;
             // 
             // txtSalario
             // 
-            this.txtSalario.Location = new System.Drawing.Point(127, 96);
+            this.txtSalario.Location = new System.Drawing.Point(117, 94);
             this.txtSalario.Name = "txtSalario";
             this.txtSalario.Size = new System.Drawing.Size(177, 20);
             this.txtSalario.TabIndex = 5;
             // 
             // txtSenha
             // 
-            this.txtSenha.Location = new System.Drawing.Point(127, 122);
+            this.txtSenha.Location = new System.Drawing.Point(117, 120);
             this.txtSenha.Name = "txtSenha";
             this.txtSenha.PasswordChar = '*';
             this.txtSenha.Size = new System.Drawing.Size(177, 20);
@@ -103,7 +104,8 @@ namespace LocadoraDeVeiculos.WindowsApp.Features.FuncionarioModule
             // 
             // dtpDataAdmissao
             // 
-            this.dtpDataAdmissao.Location = new System.Drawing.Point(127, 70);
+            this.dtpDataAdmissao.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpDataAdmissao.Location = new System.Drawing.Point(117, 68);
             this.dtpDataAdmissao.Name = "dtpDataAdmissao";
             this.dtpDataAdmissao.Size = new System.Drawing.Size(177, 20);
             this.dtpDataAdmissao.TabIndex = 7;
@@ -111,19 +113,19 @@ namespace LocadoraDeVeiculos.WindowsApp.Features.FuncionarioModule
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(105, 21);
+            this.label5.Location = new System.Drawing.Point(95, 19);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(16, 13);
+            this.label5.Size = new System.Drawing.Size(19, 13);
             this.label5.TabIndex = 10;
-            this.label5.Text = "Id";
+            this.label5.Text = "Id:";
             // 
             // txtId
             // 
             this.txtId.BackColor = System.Drawing.Color.LightGreen;
             this.txtId.Enabled = false;
-            this.txtId.Location = new System.Drawing.Point(127, 18);
+            this.txtId.Location = new System.Drawing.Point(117, 16);
             this.txtId.Name = "txtId";
-            this.txtId.Size = new System.Drawing.Size(55, 20);
+            this.txtId.Size = new System.Drawing.Size(177, 20);
             this.txtId.TabIndex = 11;
             // 
             // btnGravar
@@ -151,11 +153,21 @@ namespace LocadoraDeVeiculos.WindowsApp.Features.FuncionarioModule
             this.btnCancelar.Text = "Cancelar";
             this.btnCancelar.UseVisualStyleBackColor = false;
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(300, 97);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(21, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "R$";
+            // 
             // TelaFuncionarioForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(340, 222);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.btnCancelar);
             this.Controls.Add(this.btnGravar);
             this.Controls.Add(this.txtId);
@@ -170,6 +182,7 @@ namespace LocadoraDeVeiculos.WindowsApp.Features.FuncionarioModule
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "TelaFuncionarioForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Cadastro de Funcionários";
@@ -192,5 +205,6 @@ namespace LocadoraDeVeiculos.WindowsApp.Features.FuncionarioModule
         private System.Windows.Forms.TextBox txtId;
         private System.Windows.Forms.Button btnGravar;
         private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Label label6;
     }
 }
