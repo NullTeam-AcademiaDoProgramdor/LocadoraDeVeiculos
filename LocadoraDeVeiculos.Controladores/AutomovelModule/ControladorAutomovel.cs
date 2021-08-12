9aded67c92211e4ba1746cfd3569e53c3d8728fa
Erik Borela <erikborella@gmail.com>
2021-08-12 17:40:49 -0300

    Criado controlador para o Automovel

---
+++ LocadoraDeVeiculos.Controladores/AutomovelModule/ControladorAutomovel.cs
@@ -0,0 +1,43 @@
+﻿using LocadoraDeVeiculos.Controladores.Shared;
+using LocadoraDeVeiculos.Dominio.AutomovelModule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Controladores.AutomovelModule
+{
+    public class ControladorAutomovel : Controlador<Automovel>
+    {
+        public override string InserirNovo(Automovel registro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Editar(int id, Automovel registro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Excluir(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Existe(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Automovel SelecionarPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<Automovel> SelecionarTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
