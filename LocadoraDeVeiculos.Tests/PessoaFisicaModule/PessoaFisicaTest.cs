68af09d7ce5a7a1940023d0599a99c167e0b7032
Matheus F Lopes <matheusdefigueiredo@gmail.com>
2021-08-12 18:08:33 -0300

    Implementada classe para testar o domínio de pessoa física.

---
+++ LocadoraDeVeiculos.Tests/PessoaFisicaModule/PessoaFisicaTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace LocadoraDeVeiculos.Tests.PessoaFisicaModule
+{
+    [TestClass]
+    public class PessoaFisicaTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
