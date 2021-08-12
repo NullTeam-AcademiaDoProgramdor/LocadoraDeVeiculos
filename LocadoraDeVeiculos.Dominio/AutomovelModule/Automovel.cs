97edcd09efb2b81428b88892e3e0c8747646b44b
Erik Borela <erikborella@gmail.com>
2021-08-12 16:50:04 -0300

    Adicionada implementação de Equals e GetHashCode no dominio Automovel

--- LocadoraDeVeiculos.Dominio/AutomovelModule/Automovel.cs
+++ LocadoraDeVeiculos.Dominio/AutomovelModule/Automovel.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace LocadoraDeVeiculos.Dominio.AutomovelModule
 {
-    public class Automovel : EntidadeBase
+    public class Automovel : EntidadeBase, IEquatable<Automovel>
     {
         public string Modelo { get; }
 
@@ -32,10 +32,53 @@ namespace LocadoraDeVeiculos.Dominio.AutomovelModule
 
         public GrupoAutomovel Grupo { get; }
 
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Automovel);
+        }
+
+        public bool Equals(Automovel other)
+        {
+            return other != null &&
+                   id == other.id &&
+                   Id == other.Id &&
+                   Modelo == other.Modelo &&
+                   Marca == other.Marca &&
+                   Cor == other.Cor &&
+                   Placa == other.Placa &&
+                   Chassi == other.Chassi &&
+                   Ano == other.Ano &&
+                   NPortas == other.NPortas &&
+                   CapacidadeTanque == other.CapacidadeTanque &&
+                   TamanhoPortaMalas == other.TamanhoPortaMalas &&
+                   TipoCombustivel == other.TipoCombustivel &&
+                   EqualityComparer<GrupoAutomovel>.Default.Equals(Grupo, other.Grupo);
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = -1684633869;
+            hashCode = hashCode * -1521134295 + id.GetHashCode();
+            hashCode = hashCode * -1521134295 + Id.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Modelo);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Marca);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Cor);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Placa);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Chassi);
+            hashCode = hashCode * -1521134295 + Ano.GetHashCode();
+            hashCode = hashCode * -1521134295 + NPortas.GetHashCode();
+            hashCode = hashCode * -1521134295 + CapacidadeTanque.GetHashCode();
+            hashCode = hashCode * -1521134295 + TamanhoPortaMalas.GetHashCode();
+            hashCode = hashCode * -1521134295 + TipoCombustivel.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<GrupoAutomovel>.Default.GetHashCode(Grupo);
+            return hashCode;
+        }
 
         public override string Validar()
         {
             throw new NotImplementedException();
         }
+
+
     }
 }
