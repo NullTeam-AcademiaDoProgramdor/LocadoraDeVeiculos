9e3dd60e6bcbe0f6308de64059c889c30d4f3dbf
Matheus F Lopes <matheusdefigueiredo@gmail.com>
2021-08-12 16:43:51 -0300

    Completadas as propriedades da classe Pessoa Física.

--- LocadoraDeVeiculos.Dominio/PessoaFisicaModule/PessoaFisica.cs
+++ LocadoraDeVeiculos.Dominio/PessoaFisicaModule/PessoaFisica.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LocadoraDeVeiculos.Dominio.PessoaJuridicaModule;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,27 @@ namespace LocadoraDeVeiculos.Dominio.PessoaFisicaModule
 {
     public class PessoaFisica
     {
+        public PessoaFisica(string nome, string cPF, string rG, string cNH,
+            string telefone, string endereco, PessoaJuridica pessoaJuridica = null)
+        {
+            Nome = nome;
+            CPF = cPF;
+            RG = rG;
+            CNH = cNH;
+            Telefone = telefone;
+            Endereco = endereco;
+            PessoaJuridica = pessoaJuridica;
+        }
 
+        //Dever ter o nome, CPF, RG, CNH, telefone, endereço
+        //Pode ser ligada a uma pessoa jurídica
+
+        public string Nome { get; }
+        public string CPF { get; }
+        public string RG { get; }
+        public string CNH { get; }
+        public string Telefone { get; }
+        public string Endereco { get; }
+        public PessoaJuridica PessoaJuridica { get; }
     }
 }
