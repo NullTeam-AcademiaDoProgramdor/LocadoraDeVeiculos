0586bbbd57ebead1d61bdf8e9b0327f9409f9ebd
Erik Borela <erikborella@gmail.com>
2021-08-12 18:12:06 -0300

    Implementado operação de InserirNovo no controlador do Automovel

--- LocadoraDeVeiculos.Controladores/AutomovelModule/ControladorAutomovel.cs
+++ LocadoraDeVeiculos.Controladores/AutomovelModule/ControladorAutomovel.cs
@@ -10,9 +10,53 @@ namespace LocadoraDeVeiculos.Controladores.AutomovelModule
 {
     public class ControladorAutomovel : Controlador<Automovel>
     {
+
+        #region Queries
+        private const string sqlInserirAutomovel =
+            @"INSERT INTO [Automovel]
+	        (
+		        [placa],
+		        [chassi],
+		        [marca],
+		        [cor],
+		        [modelo],
+		        [tipoCombustivel],
+		        [capacidadeTanque],
+		        [ano],
+		        [capacidadePortaMalas],
+		        [n_portas],
+		        [cambio],
+		        [direcao],
+		        [grupo]
+	        )
+	        VALUES
+	        (
+		        @placa,
+		        @chassi,
+		        @marca,
+		        @cor,
+		        @modelo,
+		        @tipoCombustivel,
+		        @capacidadeTanque,
+		        @ano,
+		        @capacidadePortaMalas,
+		        @n_portas,
+		        @cambio,
+		        @direcao,
+		        @grupo
+	        );";
+        #endregion
+
         public override string InserirNovo(Automovel registro)
         {
-            throw new NotImplementedException();
+            string resultadoValidacao = registro.Validar();
+
+            if (resultadoValidacao == "ESTA_VALIDO")
+            {
+                registro.id = Db.Insert(sqlInserirAutomovel, ObtemParametrosAutomovel(registro));
+            }
+
+            return resultadoValidacao;
         }
 
         public override string Editar(int id, Automovel registro)
@@ -38,6 +82,29 @@ namespace LocadoraDeVeiculos.Controladores.AutomovelModule
         public override List<Automovel> SelecionarTodos()
         {
             throw new NotImplementedException();
+        }
+
+        private Dictionary<string, object> ObtemParametrosAutomovel(Automovel automovel)
+        {
+            var parametros = new Dictionary<string, object>();
+
+            parametros.Add("id", automovel.id);
+
+            parametros.Add("placa", automovel.Placa);
+            parametros.Add("chassi", automovel.Chassi);
+            parametros.Add("marca", automovel.Marca);
+            parametros.Add("cor", automovel.Cor);
+            parametros.Add("modelo", automovel.Modelo);
+            parametros.Add("tipoCombustivel", automovel.TipoCombustivel);
+            parametros.Add("capacidadeTanque", automovel.CapacidadeTanque);
+            parametros.Add("ano", automovel.Ano);
+            parametros.Add("capacidadePortaMalas", automovel.TamanhoPortaMalas);
+            parametros.Add("n_portas", automovel.Portas);
+            parametros.Add("cambio", automovel.Cambio);
+            parametros.Add("direcao", automovel.Direcao);
+            parametros.Add("grupo", automovel.Grupo.Id);
+
+            return parametros;
         }
     }
 }
