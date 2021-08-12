5f1cdeab37976d6c99467e1193ef17697edb75d4
Erik Borela <erikborella@gmail.com>
2021-08-12 16:48:26 -0300

    Criado dominio para os Automoveis

---
+++ LocadoraDeVeiculos.Dominio/AutomovelModule/Automovel.cs
@@ -0,0 +1,41 @@
+﻿using LocadoraDeVeiculos.Dominio.GrupoAutomovelModule;
+using LocadoraDeVeiculos.Dominio.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Dominio.AutomovelModule
+{
+    public class Automovel : EntidadeBase
+    {
+        public string Modelo { get; }
+
+        public string Marca { get; }
+
+        public string Cor { get; }
+        
+        public string Placa { get; }
+
+        public string Chassi { get; }
+
+        public int Ano { get; }
+
+        public int NPortas { get; }
+
+        public int CapacidadeTanque { get; }
+
+        public int TamanhoPortaMalas { get; }
+
+        public TipoCombustivelEnum TipoCombustivel { get; }
+
+        public GrupoAutomovel Grupo { get; }
+
+
+        public override string Validar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
