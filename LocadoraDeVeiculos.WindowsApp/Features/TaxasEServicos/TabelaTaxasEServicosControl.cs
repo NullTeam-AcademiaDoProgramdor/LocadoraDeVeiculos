2b5a8ecc5594e08d24c11d69892b04aa7f387ec9
Diconcili <eduardodiconcilicosta@yahoo.com.br>
2021-08-12 15:25:09 -0300

    Cifrão de reais atualizado na tabela de visualização de taxas e serivços

--- LocadoraDeVeiculos.WindowsApp/Features/TaxasEServicos/TabelaTaxasEServicosControl.cs
+++ LocadoraDeVeiculos.WindowsApp/Features/TaxasEServicos/TabelaTaxasEServicosControl.cs
@@ -50,8 +50,8 @@ namespace LocadoraDeVeiculos.WindowsApp.Features.TaxasEServicos
                 gridTaxasEServicos.Rows.Add(
                     taxaOuServico.Id,
                     taxaOuServico.Nome,
-                    taxaOuServico.Preco,
-                    (taxaOuServico.EhFixo)? "Fixo":"Não é fixo"
+                    "R$" + taxaOuServico.Preco,
+                    (taxaOuServico.EhFixo)? "Taxa fixa":"Taxa por dia"
                 );
             }
         }
