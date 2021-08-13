3d63fc87721eede133d0d730f6f90a8b8777e36d
Matheus F Lopes <matheusdefigueiredo@gmail.com>
2021-08-13 14:37:05 -0300

    Teste para telefone implementado.

--- LocadoraDeVeiculos.Tests/PessoaFisicaModule/PessoaFisicaTest.cs
+++ LocadoraDeVeiculos.Tests/PessoaFisicaModule/PessoaFisicaTest.cs
@@ -110,5 +110,17 @@ namespace LocadoraDeVeiculos.Tests.PessoaFisicaModule
             //assert
             pessoaFisica.Validar().Should().Be("A data de vencimento da CNH não pode ser aceita.");
         }
+        
+        [TestMethod]
+        public void DeveValidar_TelefoneInvalido()
+        {
+            //arange
+            PessoaJuridica pessoaJuridica = new PessoaJuridica("Matheus", "22.000.000/0001-00", "(49)000000000", "Lagi");
+            //action
+            PessoaFisica pessoaFisica = new PessoaFisica("Matheus", "123.456.789-02", "12.128.098-02", "172121891231", new DateTime(2022, 02, 20), "(49)000", "Lagi", pessoaJuridica);
+
+            //assert
+            pessoaFisica.Validar().Should().Be("Número de telefone muito pequeno.");
+        }
     }
 }
