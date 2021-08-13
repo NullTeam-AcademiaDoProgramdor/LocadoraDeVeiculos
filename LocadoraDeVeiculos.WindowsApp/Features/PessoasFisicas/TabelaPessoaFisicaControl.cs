0420a4d3dbcd15f99626996cb379859ebe71a751
Matheus F Lopes <matheusdefigueiredo@gmail.com>
2021-08-13 20:57:48 -0300

    Criada classe contol para tabela pessoa juridica.

---
+++ LocadoraDeVeiculos.WindowsApp/Features/PessoasFisicas/TabelaPessoaFisicaControl.cs
@@ -0,0 +1,24 @@
+﻿using LocadoraDeVeiculos.Controladores.PessoaFisicaModule;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LocadoraDeVeiculos.WindowsApp.Features.PessoasFisicas
+{
+    public partial class TabelaPessoaFisicaControl : UserControl
+    {
+        Subro.Controls.DataGridViewGrouper gridPessoasFisicasAgrupadas = new Subro.Controls.DataGridViewGrouper();
+        ControladorPessoaFisica controlador = new ControladorPessoaFisica();
+        public TabelaPessoaFisicaControl()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
