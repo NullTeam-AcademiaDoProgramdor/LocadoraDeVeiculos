4c8ed394f88dd56716d5debf95ca20ad0f6ac1dd
Matheus F Lopes <matheusdefigueiredo@gmail.com>
2021-08-13 21:11:50 -0300

    Tabela com control em pessoa fisica concluído.

--- LocadoraDeVeiculos.WindowsApp/Features/PessoasFisicas/TabelaPessoaFisicaControl.Designer.cs
+++ LocadoraDeVeiculos.WindowsApp/Features/PessoasFisicas/TabelaPessoaFisicaControl.Designer.cs
@@ -29,33 +29,33 @@ namespace LocadoraDeVeiculos.WindowsApp.Features.PessoasFisicas
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.gridPessoasFisicas = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.gridPessoasFisicas)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // gridPessoasFisicas
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(319, 321);
-            this.dataGridView1.TabIndex = 0;
+            this.gridPessoasFisicas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridPessoasFisicas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridPessoasFisicas.Location = new System.Drawing.Point(0, 0);
+            this.gridPessoasFisicas.Name = "gridPessoasFisicas";
+            this.gridPessoasFisicas.Size = new System.Drawing.Size(319, 321);
+            this.gridPessoasFisicas.TabIndex = 0;
             // 
             // TabelaPessoaFisicaControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.gridPessoasFisicas);
             this.Name = "TabelaPessoaFisicaControl";
             this.Size = new System.Drawing.Size(319, 321);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridPessoasFisicas)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView gridPessoasFisicas;
     }
 }
--- LocadoraDeVeiculos.WindowsApp/Features/PessoasFisicas/TabelaPessoaFisicaControl.cs
+++ LocadoraDeVeiculos.WindowsApp/Features/PessoasFisicas/TabelaPessoaFisicaControl.cs
@@ -1,4 +1,6 @@
 ﻿using LocadoraDeVeiculos.Controladores.PessoaFisicaModule;
+using LocadoraDeVeiculos.Dominio.PessoaFisicaModule;
+using LocadoraDeVeiculos.WindowsApp.Shared;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -18,7 +20,82 @@ namespace LocadoraDeVeiculos.WindowsApp.Features.PessoasFisicas
         public TabelaPessoaFisicaControl()
         {
             InitializeComponent();
-
+            gridPessoasFisicas.ConfigurarGridZebrado();
+            gridPessoasFisicas.ConfigurarGridSomenteLeitura();
+            gridPessoasFisicas.Columns.AddRange(ObterColunas());
         }
+
+        public DataGridViewColumn[] ObterColunas()
+        {
+            var colunas = new DataGridViewColumn[]
+            {
+                new DataGridViewTextBoxColumn { DataPropertyName = "Id", HeaderText = "Id"},
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "Nome", HeaderText = "Nome"},
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "CPF", HeaderText = "CPF"},
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "RG", HeaderText = "RG"},
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "CNH", HeaderText = "CNH"},
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "VencimentoCNH", HeaderText = "Vencimento da CNH"},
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "Telefone", HeaderText = "Telefone"},
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "Endereco", HeaderText = "Endereço"},
+
+                new DataGridViewTextBoxColumn { Name = "PessoaJuridica", DataPropertyName = "PessoaJuridica", HeaderText = "Empresa"}
+            };
+
+            return colunas;
+        }
+
+        public int ObtemIdSelecionado()
+        {
+            return gridPessoasFisicas.SelecionarId<int>();
+        }
+
+        public void AtualizarRegistros()
+        {
+            var pessoasFisicas = controlador.SelecionarTodos();
+
+            CarregarTabela(pessoasFisicas);
+        }
+
+        private void CarregarTabela(List<PessoaFisica> pessoasFisicas)
+        {
+            gridPessoasFisicas.DataSource = pessoasFisicas.ConvertAll(c => c as PessoaFisica);
+
+            gridPessoasFisicasAgrupadas = new Subro.Controls.DataGridViewGrouper(gridPessoasFisicas);
+        }
+
+        public void AgruparPessoasJuridicas(string campo)
+        {
+            gridPessoasFisicasAgrupadas.RemoveGrouping();
+            gridPessoasFisicasAgrupadas.SetGroupOn(campo);
+            gridPessoasFisicasAgrupadas.Options.ShowGroupName = false;
+
+            foreach (DataGridViewColumn item in gridPessoasFisicas.Columns)
+                if (item.DataPropertyName == campo)
+                    item.Visible = false;
+
+            gridPessoasFisicas.RowHeadersVisible = false;
+            gridPessoasFisicas.ClearSelection();
+        }
+
+        public void DesagruparContatos(List<PessoaFisica> pessoasFisicas)
+        {
+            var campos = new string[] { "PessoaJuridica" };
+
+            gridPessoasFisicasAgrupadas.RemoveGrouping();
+            gridPessoasFisicas.RowHeadersVisible = true;
+
+            foreach (var campo in campos)
+                foreach (DataGridViewColumn item in gridPessoasFisicas.Columns)
+                    if (item.DataPropertyName == campo)
+                        item.Visible = true;
+        }
+
     }
 }
