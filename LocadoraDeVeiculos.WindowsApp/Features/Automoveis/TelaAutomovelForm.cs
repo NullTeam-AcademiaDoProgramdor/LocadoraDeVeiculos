e337d4163e64e90f2eff3eb98c338e5242522b66
Erik Borela <erikborella@gmail.com>
2021-08-16 14:19:29 -0300

    Adicionado form de automovel

---
+++ LocadoraDeVeiculos.WindowsApp/Features/Automoveis/TelaAutomovelForm.cs
@@ -0,0 +1,33 @@
+﻿using LocadoraDeVeiculos.Dominio.AutomovelModule;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LocadoraDeVeiculos.WindowsApp.Features.Automoveis
+{
+    public partial class TelaAutomovelForm : Form
+    {
+        private Automovel automovel;
+
+        public Automovel Automovel
+        {
+            get { return automovel; }
+            set
+            {
+                automovel = value;
+                //
+            }
+        }
+
+        public TelaAutomovelForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
