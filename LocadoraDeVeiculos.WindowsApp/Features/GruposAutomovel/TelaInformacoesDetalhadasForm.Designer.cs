2e1a35e1b5e5cccd1e14a8e0cbac3358d6a3c858
Pedro Dircksen <dircksenpedro@gmail.com>
2021-08-16 15:12:27 -0300

    Implementada tela de Visualização de detalhes

---
+++ LocadoraDeVeiculos.WindowsApp/Features/GruposAutomovel/TelaInformacoesDetalhadasForm.Designer.cs
@@ -0,0 +1,266 @@
+﻿
+namespace LocadoraDeVeiculos.WindowsApp.Features.GruposAutomovel
+{
+    partial class TelaInformacoesDetalhadasForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lblPlanoDiario_PrecoKm = new System.Windows.Forms.Label();
+            this.lblPlanoDiario_PrecoDia = new System.Windows.Forms.Label();
+            this.lblPlanoControl_PrecoKmExtra = new System.Windows.Forms.Label();
+            this.lblPlanoControl_KmDisponivel = new System.Windows.Forms.Label();
+            this.lblPlanoControl_PrecoDia = new System.Windows.Forms.Label();
+            this.lblPlanoLivre_PrecoDia = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnFechar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnFechar.Location = new System.Drawing.Point(261, 372);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(75, 23);
+            this.btnFechar.TabIndex = 0;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Preço por Dia";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(7, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Preço por Km";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(7, 75);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Km Disponíveis";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(7, 46);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Preço por Dia";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(7, 104);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(74, 26);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Preço por Km \r\nExtrapolado";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(6, 35);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(72, 13);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "Preço por Dia";
+            // 
+            // lblPlanoDiario_PrecoKm
+            // 
+            this.lblPlanoDiario_PrecoKm.AutoSize = true;
+            this.lblPlanoDiario_PrecoKm.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlanoDiario_PrecoKm.Location = new System.Drawing.Point(119, 69);
+            this.lblPlanoDiario_PrecoKm.Name = "lblPlanoDiario_PrecoKm";
+            this.lblPlanoDiario_PrecoKm.Size = new System.Drawing.Size(71, 13);
+            this.lblPlanoDiario_PrecoKm.TabIndex = 11;
+            this.lblPlanoDiario_PrecoKm.Text = "Preço por Km";
+            // 
+            // lblPlanoDiario_PrecoDia
+            // 
+            this.lblPlanoDiario_PrecoDia.AutoSize = true;
+            this.lblPlanoDiario_PrecoDia.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlanoDiario_PrecoDia.Location = new System.Drawing.Point(118, 39);
+            this.lblPlanoDiario_PrecoDia.Name = "lblPlanoDiario_PrecoDia";
+            this.lblPlanoDiario_PrecoDia.Size = new System.Drawing.Size(72, 13);
+            this.lblPlanoDiario_PrecoDia.TabIndex = 10;
+            this.lblPlanoDiario_PrecoDia.Text = "Preço por Dia";
+            // 
+            // lblPlanoControl_PrecoKmExtra
+            // 
+            this.lblPlanoControl_PrecoKmExtra.AutoSize = true;
+            this.lblPlanoControl_PrecoKmExtra.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlanoControl_PrecoKmExtra.Location = new System.Drawing.Point(117, 104);
+            this.lblPlanoControl_PrecoKmExtra.Name = "lblPlanoControl_PrecoKmExtra";
+            this.lblPlanoControl_PrecoKmExtra.Size = new System.Drawing.Size(74, 26);
+            this.lblPlanoControl_PrecoKmExtra.TabIndex = 14;
+            this.lblPlanoControl_PrecoKmExtra.Text = "Preço por Km \r\nExtrapolado";
+            // 
+            // lblPlanoControl_KmDisponivel
+            // 
+            this.lblPlanoControl_KmDisponivel.AutoSize = true;
+            this.lblPlanoControl_KmDisponivel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlanoControl_KmDisponivel.Location = new System.Drawing.Point(110, 75);
+            this.lblPlanoControl_KmDisponivel.Name = "lblPlanoControl_KmDisponivel";
+            this.lblPlanoControl_KmDisponivel.Size = new System.Drawing.Size(81, 13);
+            this.lblPlanoControl_KmDisponivel.TabIndex = 13;
+            this.lblPlanoControl_KmDisponivel.Text = "Km Disponíveis";
+            // 
+            // lblPlanoControl_PrecoDia
+            // 
+            this.lblPlanoControl_PrecoDia.AutoSize = true;
+            this.lblPlanoControl_PrecoDia.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlanoControl_PrecoDia.Location = new System.Drawing.Point(119, 46);
+            this.lblPlanoControl_PrecoDia.Name = "lblPlanoControl_PrecoDia";
+            this.lblPlanoControl_PrecoDia.Size = new System.Drawing.Size(72, 13);
+            this.lblPlanoControl_PrecoDia.TabIndex = 12;
+            this.lblPlanoControl_PrecoDia.Text = "Preço por Dia";
+            // 
+            // lblPlanoLivre_PrecoDia
+            // 
+            this.lblPlanoLivre_PrecoDia.AutoSize = true;
+            this.lblPlanoLivre_PrecoDia.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlanoLivre_PrecoDia.Location = new System.Drawing.Point(107, 35);
+            this.lblPlanoLivre_PrecoDia.Name = "lblPlanoLivre_PrecoDia";
+            this.lblPlanoLivre_PrecoDia.Size = new System.Drawing.Size(72, 13);
+            this.lblPlanoLivre_PrecoDia.TabIndex = 15;
+            this.lblPlanoLivre_PrecoDia.Text = "Preço por Dia";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblPlanoDiario_PrecoKm);
+            this.groupBox1.Controls.Add(this.lblPlanoDiario_PrecoDia);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(22, 24);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(310, 100);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Plano Diário";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblPlanoControl_PrecoDia);
+            this.groupBox2.Controls.Add(this.lblPlanoControl_PrecoKmExtra);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.lblPlanoControl_KmDisponivel);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(22, 130);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(310, 141);
+            this.groupBox2.TabIndex = 17;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Plano Controlado";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.lblPlanoLivre_PrecoDia);
+            this.groupBox3.Controls.Add(this.label9);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(22, 277);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(310, 79);
+            this.groupBox3.TabIndex = 18;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Plano Livre";
+            // 
+            // TelaInformacoesDetalhadasForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 407);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox3);
+            this.Name = "TelaInformacoesDetalhadasForm";
+            this.Text = "Informações Detalhadas";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lblPlanoDiario_PrecoKm;
+        private System.Windows.Forms.Label lblPlanoDiario_PrecoDia;
+        private System.Windows.Forms.Label lblPlanoControl_PrecoKmExtra;
+        private System.Windows.Forms.Label lblPlanoControl_KmDisponivel;
+        private System.Windows.Forms.Label lblPlanoControl_PrecoDia;
+        private System.Windows.Forms.Label lblPlanoLivre_PrecoDia;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
