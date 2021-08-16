3733b66bd8e62a422130fcf82e5bbec7a175d335
Matheus F Lopes <matheusdefigueiredo@gmail.com>
2021-08-16 14:59:52 -0300

    "Aperfeiçoada" tela de agrupamento.

--- LocadoraDeVeiculos.WindowsApp/Features/PessoasFisicas/AgrupamentoPessoaFisicaForm.Designer.cs
+++ LocadoraDeVeiculos.WindowsApp/Features/PessoasFisicas/AgrupamentoPessoaFisicaForm.Designer.cs
@@ -38,30 +38,30 @@ namespace LocadoraDeVeiculos.WindowsApp.Features.PessoasJuridicas
             // rdbPessoasPorEmpresa
             // 
             this.rdbPessoasPorEmpresa.AutoSize = true;
-            this.rdbPessoasPorEmpresa.Location = new System.Drawing.Point(41, 70);
+            this.rdbPessoasPorEmpresa.Location = new System.Drawing.Point(12, 12);
             this.rdbPessoasPorEmpresa.Name = "rdbPessoasPorEmpresa";
-            this.rdbPessoasPorEmpresa.Size = new System.Drawing.Size(159, 17);
+            this.rdbPessoasPorEmpresa.Size = new System.Drawing.Size(259, 17);
             this.rdbPessoasPorEmpresa.TabIndex = 20;
             this.rdbPessoasPorEmpresa.TabStop = true;
-            this.rdbPessoasPorEmpresa.Text = "Visualizar física por empresa";
+            this.rdbPessoasPorEmpresa.Text = "Visualizar pessoas físicas agrupadas por empresa";
             this.rdbPessoasPorEmpresa.UseVisualStyleBackColor = true;
             // 
             // rdbPessoasSemOrdem
             // 
             this.rdbPessoasSemOrdem.AutoSize = true;
-            this.rdbPessoasSemOrdem.Location = new System.Drawing.Point(41, 36);
+            this.rdbPessoasSemOrdem.Location = new System.Drawing.Point(12, 45);
             this.rdbPessoasSemOrdem.Name = "rdbPessoasSemOrdem";
-            this.rdbPessoasSemOrdem.Size = new System.Drawing.Size(152, 17);
+            this.rdbPessoasSemOrdem.Size = new System.Drawing.Size(215, 17);
             this.rdbPessoasSemOrdem.TabIndex = 19;
             this.rdbPessoasSemOrdem.TabStop = true;
-            this.rdbPessoasSemOrdem.Text = "Visualizar física sem ordem";
+            this.rdbPessoasSemOrdem.Text = "Visualizar pessoas físicas desagrupadas";
             this.rdbPessoasSemOrdem.UseVisualStyleBackColor = true;
             // 
             // btnCancelar
             // 
             this.btnCancelar.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.btnCancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancelar.Location = new System.Drawing.Point(253, 106);
+            this.btnCancelar.Location = new System.Drawing.Point(172, 86);
             this.btnCancelar.Name = "btnCancelar";
             this.btnCancelar.Size = new System.Drawing.Size(75, 23);
             this.btnCancelar.TabIndex = 18;
@@ -72,7 +72,7 @@ namespace LocadoraDeVeiculos.WindowsApp.Features.PessoasJuridicas
             // 
             this.btnGravar.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.btnGravar.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.btnGravar.Location = new System.Drawing.Point(172, 106);
+            this.btnGravar.Location = new System.Drawing.Point(253, 86);
             this.btnGravar.Name = "btnGravar";
             this.btnGravar.Size = new System.Drawing.Size(75, 23);
             this.btnGravar.TabIndex = 17;
@@ -83,7 +83,7 @@ namespace LocadoraDeVeiculos.WindowsApp.Features.PessoasJuridicas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(340, 141);
+            this.ClientSize = new System.Drawing.Size(340, 121);
             this.Controls.Add(this.rdbPessoasPorEmpresa);
             this.Controls.Add(this.rdbPessoasSemOrdem);
             this.Controls.Add(this.btnCancelar);
