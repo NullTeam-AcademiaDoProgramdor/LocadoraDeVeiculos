c54b03b9f8b06c58b4b1cd53e3b441b92d2bd725
Erik Borela <erikborella@gmail.com>
2021-08-16 15:05:41 -0300

    Criado formulario automovel

--- LocadoraDeVeiculos.WindowsApp/Features/Automoveis/TelaAutomovelForm.Designer.cs
+++ LocadoraDeVeiculos.WindowsApp/Features/Automoveis/TelaAutomovelForm.Designer.cs
@@ -42,130 +42,303 @@ namespace LocadoraDeVeiculos.WindowsApp.Features.Automoveis
             this.label11 = new System.Windows.Forms.Label();
             this.label12 = new System.Windows.Forms.Label();
             this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtModelo = new System.Windows.Forms.TextBox();
+            this.txtMarca = new System.Windows.Forms.TextBox();
+            this.txtCor = new System.Windows.Forms.TextBox();
+            this.txtPlaca = new System.Windows.Forms.TextBox();
+            this.txtChassi = new System.Windows.Forms.TextBox();
+            this.txtAno = new System.Windows.Forms.TextBox();
+            this.txtPortas = new System.Windows.Forms.TextBox();
+            this.txtCapacidadeCombustivel = new System.Windows.Forms.TextBox();
+            this.txtTamanhoPortaMalas = new System.Windows.Forms.TextBox();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnGravar = new System.Windows.Forms.Button();
+            this.comboCombustivel = new System.Windows.Forms.ComboBox();
+            this.comboCambio = new System.Windows.Forms.ComboBox();
+            this.comboDirecao = new System.Windows.Forms.ComboBox();
+            this.comboGrupo = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 29);
+            this.label1.Location = new System.Drawing.Point(55, 45);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(45, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Modelo:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 51);
+            this.label2.Location = new System.Drawing.Point(60, 75);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(40, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.label2.Text = "Marca:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 73);
+            this.label3.Location = new System.Drawing.Point(74, 105);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(26, 13);
             this.label3.TabIndex = 2;
-            this.label3.Text = "label3";
+            this.label3.Text = "Cor:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 95);
+            this.label4.Location = new System.Drawing.Point(63, 135);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.Size = new System.Drawing.Size(37, 13);
             this.label4.TabIndex = 3;
-            this.label4.Text = "label4";
+            this.label4.Text = "Placa:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 161);
+            this.label5.Location = new System.Drawing.Point(60, 225);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.Size = new System.Drawing.Size(40, 13);
             this.label5.TabIndex = 4;
-            this.label5.Text = "label5";
+            this.label5.Text = "Portas:";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(12, 205);
+            this.label6.Location = new System.Drawing.Point(7, 285);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.Size = new System.Drawing.Size(93, 13);
             this.label6.TabIndex = 5;
-            this.label6.Text = "label6";
+            this.label6.Text = "Tam. Porta-Malas:";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(12, 249);
+            this.label7.Location = new System.Drawing.Point(55, 345);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.Size = new System.Drawing.Size(45, 13);
             this.label7.TabIndex = 6;
-            this.label7.Text = "label7";
+            this.label7.Text = "Cambio:";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(12, 271);
+            this.label8.Location = new System.Drawing.Point(53, 375);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.Size = new System.Drawing.Size(47, 13);
             this.label8.TabIndex = 7;
-            this.label8.Text = "label8";
+            this.label8.Text = "Direção:";
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(12, 293);
+            this.label9.Location = new System.Drawing.Point(61, 405);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.Size = new System.Drawing.Size(39, 13);
             this.label9.TabIndex = 8;
-            this.label9.Text = "label9";
+            this.label9.Text = "Grupo:";
             // 
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(12, 139);
+            this.label10.Location = new System.Drawing.Point(71, 195);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(41, 13);
+            this.label10.Size = new System.Drawing.Size(29, 13);
             this.label10.TabIndex = 9;
-            this.label10.Text = "label10";
+            this.label10.Text = "Ano:";
             // 
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(12, 183);
+            this.label11.Location = new System.Drawing.Point(9, 255);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(41, 13);
+            this.label11.Size = new System.Drawing.Size(91, 13);
             this.label11.TabIndex = 10;
-            this.label11.Text = "label11";
+            this.label11.Text = "Cap. combustivel:";
             // 
             // label12
             // 
             this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(12, 227);
+            this.label12.Location = new System.Drawing.Point(33, 315);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(41, 13);
+            this.label12.Size = new System.Drawing.Size(67, 13);
             this.label12.TabIndex = 11;
-            this.label12.Text = "label12";
+            this.label12.Text = "Combustivel:";
             // 
             // label13
             // 
             this.label13.AutoSize = true;
-            this.label13.Location = new System.Drawing.Point(12, 117);
+            this.label13.Location = new System.Drawing.Point(59, 165);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(41, 13);
             this.label13.TabIndex = 12;
-            this.label13.Text = "label13";
+            this.label13.Text = "Chassi:";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(81, 15);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(19, 13);
+            this.label14.TabIndex = 13;
+            this.label14.Text = "Id:";
+            // 
+            // txtId
+            // 
+            this.txtId.BackColor = System.Drawing.Color.LightGreen;
+            this.txtId.Enabled = false;
+            this.txtId.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtId.Location = new System.Drawing.Point(106, 12);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(222, 20);
+            this.txtId.TabIndex = 14;
+            // 
+            // txtModelo
+            // 
+            this.txtModelo.Location = new System.Drawing.Point(106, 42);
+            this.txtModelo.Name = "txtModelo";
+            this.txtModelo.Size = new System.Drawing.Size(222, 20);
+            this.txtModelo.TabIndex = 15;
+            // 
+            // txtMarca
+            // 
+            this.txtMarca.Location = new System.Drawing.Point(106, 72);
+            this.txtMarca.Name = "txtMarca";
+            this.txtMarca.Size = new System.Drawing.Size(222, 20);
+            this.txtMarca.TabIndex = 16;
+            // 
+            // txtCor
+            // 
+            this.txtCor.Location = new System.Drawing.Point(106, 102);
+            this.txtCor.Name = "txtCor";
+            this.txtCor.Size = new System.Drawing.Size(222, 20);
+            this.txtCor.TabIndex = 17;
+            // 
+            // txtPlaca
+            // 
+            this.txtPlaca.Location = new System.Drawing.Point(106, 132);
+            this.txtPlaca.Name = "txtPlaca";
+            this.txtPlaca.Size = new System.Drawing.Size(222, 20);
+            this.txtPlaca.TabIndex = 18;
+            // 
+            // txtChassi
+            // 
+            this.txtChassi.Location = new System.Drawing.Point(106, 162);
+            this.txtChassi.Name = "txtChassi";
+            this.txtChassi.Size = new System.Drawing.Size(222, 20);
+            this.txtChassi.TabIndex = 19;
+            // 
+            // txtAno
+            // 
+            this.txtAno.Location = new System.Drawing.Point(106, 192);
+            this.txtAno.Name = "txtAno";
+            this.txtAno.Size = new System.Drawing.Size(222, 20);
+            this.txtAno.TabIndex = 20;
+            // 
+            // txtPortas
+            // 
+            this.txtPortas.Location = new System.Drawing.Point(106, 222);
+            this.txtPortas.Name = "txtPortas";
+            this.txtPortas.Size = new System.Drawing.Size(222, 20);
+            this.txtPortas.TabIndex = 21;
+            // 
+            // txtCapacidadeCombustivel
+            // 
+            this.txtCapacidadeCombustivel.Location = new System.Drawing.Point(106, 252);
+            this.txtCapacidadeCombustivel.Name = "txtCapacidadeCombustivel";
+            this.txtCapacidadeCombustivel.Size = new System.Drawing.Size(222, 20);
+            this.txtCapacidadeCombustivel.TabIndex = 22;
+            // 
+            // txtTamanhoPortaMalas
+            // 
+            this.txtTamanhoPortaMalas.Location = new System.Drawing.Point(106, 282);
+            this.txtTamanhoPortaMalas.Name = "txtTamanhoPortaMalas";
+            this.txtTamanhoPortaMalas.Size = new System.Drawing.Size(222, 20);
+            this.txtTamanhoPortaMalas.TabIndex = 23;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnCancelar.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnCancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancelar.Location = new System.Drawing.Point(172, 440);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 29;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            // 
+            // btnGravar
+            // 
+            this.btnGravar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnGravar.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnGravar.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnGravar.Location = new System.Drawing.Point(253, 440);
+            this.btnGravar.Name = "btnGravar";
+            this.btnGravar.Size = new System.Drawing.Size(75, 23);
+            this.btnGravar.TabIndex = 28;
+            this.btnGravar.Text = "Gravar";
+            this.btnGravar.UseVisualStyleBackColor = false;
+            this.btnGravar.Click += new System.EventHandler(this.btnGravar_Click);
+            // 
+            // comboCombustivel
+            // 
+            this.comboCombustivel.FormattingEnabled = true;
+            this.comboCombustivel.Location = new System.Drawing.Point(106, 312);
+            this.comboCombustivel.Name = "comboCombustivel";
+            this.comboCombustivel.Size = new System.Drawing.Size(222, 21);
+            this.comboCombustivel.TabIndex = 30;
+            // 
+            // comboCambio
+            // 
+            this.comboCambio.FormattingEnabled = true;
+            this.comboCambio.Location = new System.Drawing.Point(106, 342);
+            this.comboCambio.Name = "comboCambio";
+            this.comboCambio.Size = new System.Drawing.Size(222, 21);
+            this.comboCambio.TabIndex = 31;
+            // 
+            // comboDirecao
+            // 
+            this.comboDirecao.FormattingEnabled = true;
+            this.comboDirecao.Location = new System.Drawing.Point(106, 372);
+            this.comboDirecao.Name = "comboDirecao";
+            this.comboDirecao.Size = new System.Drawing.Size(222, 21);
+            this.comboDirecao.TabIndex = 32;
+            // 
+            // comboGrupo
+            // 
+            this.comboGrupo.FormattingEnabled = true;
+            this.comboGrupo.Location = new System.Drawing.Point(106, 402);
+            this.comboGrupo.Name = "comboGrupo";
+            this.comboGrupo.Size = new System.Drawing.Size(222, 21);
+            this.comboGrupo.TabIndex = 33;
             // 
             // TelaAutomovelForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(340, 343);
+            this.ClientSize = new System.Drawing.Size(340, 475);
+            this.Controls.Add(this.comboGrupo);
+            this.Controls.Add(this.comboDirecao);
+            this.Controls.Add(this.comboCambio);
+            this.Controls.Add(this.comboCombustivel);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnGravar);
+            this.Controls.Add(this.txtTamanhoPortaMalas);
+            this.Controls.Add(this.txtCapacidadeCombustivel);
+            this.Controls.Add(this.txtPortas);
+            this.Controls.Add(this.txtAno);
+            this.Controls.Add(this.txtChassi);
+            this.Controls.Add(this.txtPlaca);
+            this.Controls.Add(this.txtCor);
+            this.Controls.Add(this.txtMarca);
+            this.Controls.Add(this.txtModelo);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label14);
             this.Controls.Add(this.label13);
             this.Controls.Add(this.label12);
             this.Controls.Add(this.label11);
@@ -201,5 +374,22 @@ namespace LocadoraDeVeiculos.WindowsApp.Features.Automoveis
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.Label label12;
         private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtModelo;
+        private System.Windows.Forms.TextBox txtMarca;
+        private System.Windows.Forms.TextBox txtCor;
+        private System.Windows.Forms.TextBox txtPlaca;
+        private System.Windows.Forms.TextBox txtChassi;
+        private System.Windows.Forms.TextBox txtAno;
+        private System.Windows.Forms.TextBox txtPortas;
+        private System.Windows.Forms.TextBox txtCapacidadeCombustivel;
+        private System.Windows.Forms.TextBox txtTamanhoPortaMalas;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnGravar;
+        private System.Windows.Forms.ComboBox comboCombustivel;
+        private System.Windows.Forms.ComboBox comboCambio;
+        private System.Windows.Forms.ComboBox comboDirecao;
+        private System.Windows.Forms.ComboBox comboGrupo;
     }
 }
