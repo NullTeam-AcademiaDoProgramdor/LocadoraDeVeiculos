8d18e9a4e81fc105b8bf6bde1cbd068c50d4538c
Erik Borela <erikborella@gmail.com>
2021-08-17 16:04:08 -0300

    Adicionado operação para buscar imagens

--- LocadoraDeVeiculos.Controladores/AutomovelModule/ControladorFotos.cs
+++ LocadoraDeVeiculos.Controladores/AutomovelModule/ControladorFotos.cs
@@ -5,11 +5,27 @@ using System.Text;
 using System.Threading.Tasks;
 using LocadoraDeVeiculos.Controladores.Shared;
 using System.Drawing;
+using System.IO;
+using System.Data;
 
 namespace LocadoraDeVeiculos.Controladores.AutomovelModule
 {
     public class ControladorFotos
     {
+        internal class EntidadeImage
+        {
+            public int id;
+            public Image foto;
+            public int automovel;
+
+            public EntidadeImage(int id, Image foto, int automovel)
+            {
+                this.id = id;
+                this.foto = foto;
+                this.automovel = automovel;
+            }
+        }
+
         public const string sqlInserirImagens =
             @"INSERT INTO [FotoAutomovel]
             (
@@ -22,6 +38,18 @@ namespace LocadoraDeVeiculos.Controladores.AutomovelModule
 	            @automovel
             );";
 
+        public const string sqlBuscarImagens =
+            @"SELECT
+	            [id],
+	            [foto],
+	            [automovel]
+            FROM
+	            [FotoAutomovel]
+            WHERE
+	            [automovel] = @automovel;";
+
+     
+
         public void Inserir(Image[] imagens, int automovelId)
         {
             foreach(Image foto in imagens)
@@ -30,14 +58,52 @@ namespace LocadoraDeVeiculos.Controladores.AutomovelModule
             }
         }
 
+        public Image[] Buscar(int automovelId)
+        {
+            Dictionary<string, object> parametros = new Dictionary<string, object>()
+            {
+                {"automovel", automovelId }
+            };
+
+            List<EntidadeImage> entidadeImages = Db.GetAll(sqlBuscarImagens, ConverterEmEntidadeImage, parametros);
+
+            return entidadeImages.Select(e => e.foto).ToArray();
+        }
+
         private Dictionary<string, object> ObtemParametrosFoto(Image foto, int automovelId)
         {
             Dictionary<string, object> parametros = new Dictionary<string, object>();
 
-            parametros.Add("foto", foto);
+            parametros.Add("foto", ImageToByteArray(foto));
             parametros.Add("automovel", automovelId);
 
             return parametros;
+        }
+
+        private EntidadeImage ConverterEmEntidadeImage(IDataReader reader)
+        {
+            int id = Convert.ToInt32(reader["id"]);
+            int automovel = Convert.ToInt32(reader["automovel"]);
+            Image foto = ByteArrayToImage((byte[])reader["foto"]);
+
+            return new EntidadeImage(id, foto, automovel);
+        }
+
+        private byte[] ImageToByteArray(Image image)
+        {
+            using (var ms = new MemoryStream())
+            {
+                image.Save(ms, image.RawFormat);
+                return ms.ToArray();
+            }
+        }
+
+        private Image ByteArrayToImage(byte[] imageBytes)
+        {
+            using (var ms = new MemoryStream(imageBytes))
+            {
+                return Image.FromStream(ms);
+            }
         }
     }
 }
