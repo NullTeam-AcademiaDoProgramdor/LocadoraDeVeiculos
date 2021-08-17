1fd37cbd097d23264efed36e5bfb63489df3e816
Pedro Dircksen <dircksenpedro@gmail.com>
2021-08-17 15:30:08 -0300

    implementadas propriedades da locação

--- LocadoraDeVeiculos.Dominio/LocacaoModule/Locacao.cs
+++ LocadoraDeVeiculos.Dominio/LocacaoModule/Locacao.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using LocadoraDeVeiculos.Dominio.AutomovelModule;
+using LocadoraDeVeiculos.Dominio.FuncionarioModule;
+using LocadoraDeVeiculos.Dominio.PessoaFisicaModule;
+using LocadoraDeVeiculos.Dominio.Shared;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,13 +10,34 @@ using System.Threading.Tasks;
 
 namespace LocadoraDeVeiculos.Dominio.LocacaoModule
 {
-    class Locacao
+    public class Locacao : EntidadeBase
     {
-
-        public Locacao()
+        public Locacao(PessoaFisica condutor, Automovel automovel, Funcionario funcionario, DateTime dataSaida, DateTime dataDevolucaoEsperada, int caucao, int kmAutomovelIncial)
         {
-
+            this.condutor = condutor;
+            this.automovel = automovel;
+            this.funcionario = funcionario;
+            this.dataSaida = dataSaida;
+            this.dataDevolucaoEsperada = dataDevolucaoEsperada;
+            this.caucao = caucao;
+            this.kmAutomovelIncial = kmAutomovelIncial;
         }
 
+        public PessoaFisica condutor { get; }
+        public Automovel automovel { get; }
+        public Funcionario funcionario { get; }
+        public DateTime dataSaida { get; }
+        public DateTime dataDevolucaoEsperada { get; }
+        public DateTime dataDevolucao { get; }
+        public int caucao { get; }      
+        public int kmAutomovelIncial { get; }      
+        public int kmAutomovelFinal { get; }      
+        public int porcentagemFinalCombustivel { get; } 
+
+
+        public override string Validar()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
