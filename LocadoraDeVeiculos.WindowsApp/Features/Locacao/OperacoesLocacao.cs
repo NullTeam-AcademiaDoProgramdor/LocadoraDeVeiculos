cfdd6735350a1949fc7c2e07d150b8410874d1b9
Diconcili <eduardodiconcilicosta@yahoo.com.br>
2021-08-17 15:47:18 -0300

    Classe de operações da locação criada, métodos ainda não adicionados

---
+++ LocadoraDeVeiculos.WindowsApp/Features/Locacao/OperacoesLocacao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LocadoraDeVeiculos.WindowsApp.Shared;
+using LocadoraDeVeiculos.Dominio.LocacaoModule;
+
+
+namespace LocadoraDeVeiculos.WindowsApp.Features.Locacao
+{
+    class OperacoesLocacao : ICadastravel
+    {
+        private readonly ControladorPessoaFisica controlador = null;      
+        private readonly TabelaLocacao tabelaLocacao = null;
+
+        public OperacoesPessoaFisica(ControladorPessoaFisica ctrl)
+        {
+            controlador = ctrl;
+            tabelaLocacao = new TabelaLocacao();           
+        }
+
+
+    }
+}
