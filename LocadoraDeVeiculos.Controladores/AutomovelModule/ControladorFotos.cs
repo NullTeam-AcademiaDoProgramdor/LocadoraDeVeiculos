b5b68941e98ff18f15e909f4d08cd2a14f7de9f5
Erik Borela <erikborella@gmail.com>
2021-08-17 15:39:51 -0300

    Adicionado operação de inserir uma foto no controlador das fotos

--- LocadoraDeVeiculos.Controladores/AutomovelModule/ControladorFotos.cs
+++ LocadoraDeVeiculos.Controladores/AutomovelModule/ControladorFotos.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LocadoraDeVeiculos.Controladores.Shared;
+using System.Drawing;
 
 namespace LocadoraDeVeiculos.Controladores.AutomovelModule
 {
-    class ControladorFotos
+    public class ControladorFotos
     {
+        public const string sqlInserirImagens =
+            @"INSERT INTO [FotoAutomovel]
+            (
+	            [foto],
+	            [automovel]
+            ) 
+            VALUES
+            (
+	            @foto,
+	            @automovel
+            );";
+
+        public void Inserir(Image[] imagens, int automovelId)
+        {
+            foreach(Image foto in imagens)
+            {
+                Db.Insert(sqlInserirImagens, ObtemParametrosFoto(foto, automovelId));
+            }
+        }
+
+        private Dictionary<string, object> ObtemParametrosFoto(Image foto, int automovelId)
+        {
+            Dictionary<string, object> parametros = new Dictionary<string, object>();
+
+            parametros.Add("foto", foto);
+            parametros.Add("automovel", automovelId);
+
+            return parametros;
+        }
     }
 }
