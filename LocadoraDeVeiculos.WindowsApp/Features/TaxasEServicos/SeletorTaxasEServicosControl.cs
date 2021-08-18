6b30dae9681de700ee5bc49bd52adb12b1e47365
Erik Borela <erikborella@gmail.com>
2021-08-18 15:19:25 -0300

    Criado componente para selecionar taxas E servicos

---
+++ LocadoraDeVeiculos.WindowsApp/Features/TaxasEServicos/SeletorTaxasEServicosControl.cs
@@ -0,0 +1,55 @@
+﻿using LocadoraDeVeiculos.Dominio.TaxasEServicosModule;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LocadoraDeVeiculos.WindowsApp.Features.TaxasEServicos
+{
+    public partial class SeletorTaxasEServicosControl : UserControl
+    {
+        private TaxaEServico[] taxasEServicos;
+        public TaxaEServico[] TaxasEServicosSelecionados { get; private set; }
+        private SeletorTaxasEServicosControl()
+        {
+            InitializeComponent();
+        }
+
+        public SeletorTaxasEServicosControl(TaxaEServico[] taxasEServicos)
+        {
+            InitializeComponent();
+
+            this.taxasEServicos = taxasEServicos;
+        }
+
+        private void btnSelecionarAdicionais_Click(object sender, EventArgs e)
+        {
+            SeletorTaxasEServicosForm tela 
+                = new SeletorTaxasEServicosForm(this.taxasEServicos);
+
+            if (tela.ShowDialog() == DialogResult.OK)
+            {
+                this.TaxasEServicosSelecionados = tela.TaxasEServicosSelecionados;
+                GerarTextoDeSelecioandos(this.TaxasEServicosSelecionados);
+            }
+        }
+
+        private void GerarTextoDeSelecioandos(TaxaEServico[] taxasSelecioandas)
+        {
+            StringBuilder str = new StringBuilder();
+
+            foreach (var taxaESevico in taxasEServicos)
+            {
+                str.Append(taxaESevico.Nome);
+                str.Append(", ");
+            }
+
+            labelSelecionados.Text = str.ToString();
+        }
+    }
+}
---
+++ LocadoraDeVeiculos.WindowsApp/Features/TaxasEServicos/SeletorTaxasEServicosForm.cs
@@ -0,0 +1,87 @@
+﻿using LocadoraDeVeiculos.Dominio.TaxasEServicosModule;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LocadoraDeVeiculos.WindowsApp.Features.TaxasEServicos
+{
+    public partial class SeletorTaxasEServicosForm : Form
+    {
+        private TaxaEServico[] taxaEServicos;
+        public TaxaEServico[] TaxasEServicosSelecionados { get; set; }
+
+        private SeletorTaxasEServicosForm()
+        {
+            InitializeComponent();
+        }
+
+        public SeletorTaxasEServicosControl(TaxaEServico[] taxasEServicos)
+        {
+            InitializeComponent();
+
+            this.taxaEServicos = taxasEServicos;
+
+            CriarTabelaDeCheckboxs();
+        }
+
+        private void CriarTabelaDeCheckboxs()
+        {
+            tableTaxaEServicos.Controls.Clear();
+            tableTaxaEServicos.RowStyles.Clear();
+            tableTaxaEServicos.RowCount = taxaEServicos.Length;
+
+            for (int i = 0; i < taxaEServicos.Length; i++)
+            {
+                TaxaEServico taxaEServico = taxaEServicos[i];
+
+                tableTaxaEServicos.RowStyles.Add(new RowStyle());
+
+                tableTaxaEServicos.Controls.Add(
+                    CriarCheckBox(taxaEServico.Nome, i),
+                    0,
+                    i
+                );
+            }
+        }
+
+        private CheckBox CriarCheckBox(string nome, int i)
+        {
+            CheckBox checkBox = new CheckBox();
+
+            checkBox.AutoSize = true;
+            checkBox.Name = $"checkBox{i}";
+            checkBox.TabIndex = i;
+            checkBox.Text = nome;
+            checkBox.UseVisualStyleBackColor = true;
+
+            return checkBox;
+        }
+
+        private void btnGravar_Click(object sender, EventArgs e)
+        {
+            var controlsList = tableTaxaEServicos.Controls;
+            var tempTaxasEServicos = new List<TaxaEServico>();
+
+            for (int i = 0; i < controlsList.Count; i++)
+            {
+                Control control = controlsList[i];
+
+                if (!(control is CheckBox))
+                    continue;
+
+                CheckBox check = control as CheckBox;
+
+                if (check.Checked)
+                    tempTaxasEServicos.Add(this.taxaEServicos[i]);
+            }
+
+            this.TaxasEServicosSelecionados = tempTaxasEServicos.ToArray();
+        }
+    }
+}
