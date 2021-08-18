33df8ce4a44bbfdb906cf3a91d975dd0a2a0e270
Matheus F Lopes <matheusdefigueiredo@gmail.com>
2021-08-18 15:48:22 -0300

    Adicionado plano no construtor. Removido plano do equals e hash code.

--- LocadoraDeVeiculos.Dominio/LocacaoModule/Locacao.cs
+++ LocadoraDeVeiculos.Dominio/LocacaoModule/Locacao.cs
@@ -11,9 +11,9 @@ using System.Threading.Tasks;
 
 namespace LocadoraDeVeiculos.Dominio.LocacaoModule
 {
-    public class Locacao : EntidadeBase
+    public class Locacao : EntidadeBase, IEquatable<Locacao>
     {
-        public Locacao(PessoaFisica condutor, Automovel automovel, Funcionario funcionario, DateTime dataSaida, DateTime dataDevolucaoEsperada, int caucao, int kmAutomovelIncial, TaxaEServico[] taxaEServicos = null)
+        public Locacao(PessoaFisica condutor, Automovel automovel, Funcionario funcionario, DateTime dataSaida, DateTime dataDevolucaoEsperada, int caucao, int kmAutomovelIncial, int planoSelecionado, TaxaEServico[] taxaEServicos = null)
         {
             this.Condutor = condutor;
             this.Automovel = automovel;
@@ -21,6 +21,7 @@ namespace LocadoraDeVeiculos.Dominio.LocacaoModule
             this.DataSaida = dataSaida;
             this.DataDevolucaoEsperada = dataDevolucaoEsperada;
             this.Caucao = caucao;
+            this.PlanoSelecionado = planoSelecionado;
             this.KmAutomovelIncial = kmAutomovelIncial;
             this.DataDevolucao = null;
             this.KmAutomovelFinal = null;
@@ -43,7 +44,51 @@ namespace LocadoraDeVeiculos.Dominio.LocacaoModule
         public int Caucao { get; }      
         public int KmAutomovelIncial { get; }      
         public int? KmAutomovelFinal { get; }      
-        public int? PorcentagemFinalCombustivel { get; } 
+        public int? PorcentagemFinalCombustivel { get; }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Locacao);
+        }
+
+        public bool Equals(Locacao other)
+        {
+            return other != null &&
+                   id == other.id &&
+                   Id == other.Id &&
+                   EqualityComparer<PessoaFisica>.Default.Equals(Condutor, other.Condutor) &&
+                   EqualityComparer<Automovel>.Default.Equals(Automovel, other.Automovel) &&
+                   EqualityComparer<Funcionario>.Default.Equals(Funcionario, other.Funcionario) &&
+                   DataSaida == other.DataSaida &&
+                   DataDevolucaoEsperada == other.DataDevolucaoEsperada &&
+                   DataDevolucao == other.DataDevolucao &&
+                   PlanoSelecionado == other.PlanoSelecionado &&
+                   //EqualityComparer<TaxaEServico[]>.Default.Equals(TaxasEServicos, other.TaxasEServicos) &&
+                   Caucao == other.Caucao &&
+                   KmAutomovelIncial == other.KmAutomovelIncial &&
+                   KmAutomovelFinal == other.KmAutomovelFinal &&
+                   PorcentagemFinalCombustivel == other.PorcentagemFinalCombustivel;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = -725128496;
+            hashCode = hashCode * -1521134295 + id.GetHashCode();
+            hashCode = hashCode * -1521134295 + Id.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<PessoaFisica>.Default.GetHashCode(Condutor);
+            hashCode = hashCode * -1521134295 + EqualityComparer<Automovel>.Default.GetHashCode(Automovel);
+            hashCode = hashCode * -1521134295 + EqualityComparer<Funcionario>.Default.GetHashCode(Funcionario);
+            hashCode = hashCode * -1521134295 + DataSaida.GetHashCode();
+            hashCode = hashCode * -1521134295 + DataDevolucaoEsperada.GetHashCode();
+            hashCode = hashCode * -1521134295 + DataDevolucao.GetHashCode();
+            hashCode = hashCode * -1521134295 + PlanoSelecionado.GetHashCode();
+            //hashCode = hashCode * -1521134295 + EqualityComparer<TaxaEServico[]>.Default.GetHashCode(TaxasEServicos);
+            hashCode = hashCode * -1521134295 + Caucao.GetHashCode();
+            hashCode = hashCode * -1521134295 + KmAutomovelIncial.GetHashCode();
+            hashCode = hashCode * -1521134295 + KmAutomovelFinal.GetHashCode();
+            hashCode = hashCode * -1521134295 + PorcentagemFinalCombustivel.GetHashCode();
+            return hashCode;
+        }
 
         public override string Validar()
         {
@@ -69,5 +114,7 @@ namespace LocadoraDeVeiculos.Dominio.LocacaoModule
 
             return resultadoValidacao;
         }
+
+
     }
 }
