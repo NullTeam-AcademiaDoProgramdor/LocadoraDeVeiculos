d58432c3f662991b4a183394a9f359573603e762
Erik Borela <erikborella@gmail.com>
2021-08-18 13:56:56 -0300

    Adicionados campos adicionais no dominio da Locação

--- LocadoraDeVeiculos.Dominio/LocacaoModule/Locacao.cs
+++ LocadoraDeVeiculos.Dominio/LocacaoModule/Locacao.cs
@@ -2,6 +2,7 @@
 using LocadoraDeVeiculos.Dominio.FuncionarioModule;
 using LocadoraDeVeiculos.Dominio.PessoaFisicaModule;
 using LocadoraDeVeiculos.Dominio.Shared;
+using LocadoraDeVeiculos.Dominio.TaxasEServicosModule;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,49 +13,57 @@ namespace LocadoraDeVeiculos.Dominio.LocacaoModule
 {
     public class Locacao : EntidadeBase
     {
-        public Locacao(PessoaFisica condutor, Automovel automovel, Funcionario funcionario, DateTime dataSaida, DateTime dataDevolucaoEsperada, int caucao, int kmAutomovelIncial)
+        public Locacao(PessoaFisica condutor, Automovel automovel, Funcionario funcionario, DateTime dataSaida, DateTime dataDevolucaoEsperada, int caucao, int kmAutomovelIncial, TaxaEServico[] taxaEServicos)
         {
-            this.condutor = condutor;
-            this.automovel = automovel;
-            this.funcionario = funcionario;
-            this.dataSaida = dataSaida;
-            this.dataDevolucaoEsperada = dataDevolucaoEsperada;
-            this.caucao = caucao;
-            this.kmAutomovelIncial = kmAutomovelIncial;
-            this.dataDevolucao = null;
-            this.kmAutomovelFinal = null;
-            this.porcentagemFinalCombustivel = null;
+            this.Condutor = condutor;
+            this.Automovel = automovel;
+            this.Funcionario = funcionario;
+            this.DataSaida = dataSaida;
+            this.DataDevolucaoEsperada = dataDevolucaoEsperada;
+            this.Caucao = caucao;
+            this.KmAutomovelIncial = kmAutomovelIncial;
+            this.DataDevolucao = null;
+            this.KmAutomovelFinal = null;
+            this.PorcentagemFinalCombustivel = null;
+
+            if (taxaEServicos == null)
+                this.TaxasEServicos = new TaxaEServico[0];
+            else
+                this.TaxasEServicos = taxaEServicos;
         }
 
-        public PessoaFisica condutor { get; }
-        public Automovel automovel { get; }
-        public Funcionario funcionario { get; }
-        public DateTime dataSaida { get; }
-        public DateTime dataDevolucaoEsperada { get; }
-        public DateTime? dataDevolucao { get; }
-        public int caucao { get; }      
-        public int kmAutomovelIncial { get; }      
-        public int? kmAutomovelFinal { get; }      
-        public int? porcentagemFinalCombustivel { get; } 
+        public PessoaFisica Condutor { get; }
+        public Automovel Automovel { get; }
+        public Funcionario Funcionario { get; }
+        public DateTime DataSaida { get; }
+        public DateTime DataDevolucaoEsperada { get; }
+        public DateTime? DataDevolucao { get; }
+        public int PlanoSelecionado { get; }
+        public TaxaEServico[] TaxasEServicos { get; }
+        public int Caucao { get; }      
+        public int KmAutomovelIncial { get; }      
+        public int? KmAutomovelFinal { get; }      
+        public int? PorcentagemFinalCombustivel { get; } 
+
 
 
         public override string Validar()
         {
             string resultadoValidacao = "";
 
-            if (condutor == null)
+            if (Condutor == null)
                 resultadoValidacao = "Selecione um condutor";
             
-            if (automovel == null)
+            if (Automovel == null)
                 resultadoValidacao += QuebraDeLinha(resultadoValidacao) + "Selecione um automóvel";
 
-            if(caucao <= 0)
+            if(Caucao <= 0)
                 resultadoValidacao += QuebraDeLinha(resultadoValidacao) + "O valor de caução não pode ser menor ou igual a 0";
 
-            if (kmAutomovelIncial <= 0)
+            if (KmAutomovelIncial <= 0)
                 resultadoValidacao += QuebraDeLinha(resultadoValidacao) + "Insira um valor válido para a quilometragem inicial";
 
-            if(dataDevolucaoEsperada <= dataSaida)
+            if(DataDevolucaoEsperada <= DataSaida)
                 resultadoValidacao += QuebraDeLinha(resultadoValidacao) + "A data de devolução esperada não pode ser menor ou igual que a data de saída";
 
             if (resultadoValidacao == "")
