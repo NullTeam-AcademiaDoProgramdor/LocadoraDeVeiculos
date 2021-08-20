0748afeb9e655d1c51a01cfc7043b803088df8c5
Matheus F Lopes <matheusdefigueiredo@gmail.com>
2021-08-20 14:51:46 -0300

    Corrigida operação para devolução de locação.

--- LocadoraDeVeiculos.WindowsApp/Features/LocacaoModule/OperacoesLocacao.cs
+++ LocadoraDeVeiculos.WindowsApp/Features/LocacaoModule/OperacoesLocacao.cs
@@ -180,11 +180,11 @@ namespace LocadoraDeVeiculos.WindowsApp.Features.LocacaoModule
 
             if (tela.ShowDialog() == DialogResult.OK)
             {
-                controlador.Editar(id, tela.Locacao);
+                controlador.Devolver(id, tela.Locacao);
 
                 tabelaLocacao.AtualizarRegistros(controlador.SelecionarTodos());
 
-                TelaPrincipalForm.Instancia.AtualizarRodape($"Locação do automóvel: [{tela.Locacao.Automovel}] editada com sucesso");
+                TelaPrincipalForm.Instancia.AtualizarRodape($"Devolução do automóvel: [{tela.Locacao.Automovel}] concluída com sucesso");
             }
         }
 
