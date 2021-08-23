0e95ce4a5b2007a2613f4aee1013da5dc9cbac78
Erik Borela <erikborella@gmail.com>
2021-08-23 12:34:34 -0300

    Banco de dados atualizado

--- LocadoraDeVeiculos.DB/dbo/Tables/TaxasEServicosUsadas.sql
+++ LocadoraDeVeiculos.DB/dbo/Tables/TaxasEServicosUsadas.sql
@@ -3,7 +3,9 @@
     [taxaEServico] INT NOT NULL,
     [locacao]      INT NOT NULL,
     CONSTRAINT [PK_TaxasEServicosUsadas] PRIMARY KEY CLUSTERED ([id] ASC),
-    CONSTRAINT [FK_TaxasEServicosUsadas_Locacao] FOREIGN KEY ([locacao]) REFERENCES [dbo].[Locacao] ([id]),
-    CONSTRAINT [FK_TaxasEServicosUsadas_TaxaEServico] FOREIGN KEY ([taxaEServico]) REFERENCES [dbo].[TaxaEServico] ([id])
+    CONSTRAINT [FK_TaxasEServicosUsadas_Locacao] FOREIGN KEY ([locacao]) REFERENCES [dbo].[Locacao] ([id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_TaxasEServicosUsadas_TaxaEServico] FOREIGN KEY ([taxaEServico]) REFERENCES [dbo].[TaxaEServico] ([id]) ON DELETE CASCADE
 );
+
+
 
