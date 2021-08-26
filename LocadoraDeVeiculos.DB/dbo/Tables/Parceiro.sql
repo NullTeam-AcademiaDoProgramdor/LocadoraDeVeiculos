9f14e874f679c221ae122bf233558d6682a0e9d2
Matheus F Lopes <matheusdefigueiredo@gmail.com>
2021-08-26 16:57:20 -0300

    Identitty adicionado para Tabela Parceiro no Banco.

--- LocadoraDeVeiculos.DB/dbo/Tables/Parceiro.sql
+++ LocadoraDeVeiculos.DB/dbo/Tables/Parceiro.sql
@@ -1,6 +1,8 @@
 ﻿CREATE TABLE [dbo].[Parceiro] (
-    [id]   INT           NOT NULL,
+    [id]   INT           IDENTITY (1, 1) NOT NULL,
     [nome] VARCHAR (100) NOT NULL,
     CONSTRAINT [PK_Parceiro] PRIMARY KEY CLUSTERED ([id] ASC)
 );
+
+
 
