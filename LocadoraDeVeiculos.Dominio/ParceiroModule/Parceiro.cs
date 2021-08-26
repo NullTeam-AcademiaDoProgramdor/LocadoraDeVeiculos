32801c4dda62f7360eac1f58d6bfbfe0f8ea180b
Matheus F Lopes <matheusdefigueiredo@gmail.com>
2021-08-26 16:11:39 -0300

    Dominio parceiro sem validação.

---
+++ LocadoraDeVeiculos.Dominio/ParceiroModule/Parceiro.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Dominio.ParceiroModule
+{
+    public class Parceiro : IEquatable<Parceiro>
+    {
+        public Parceiro(string nome)
+        {
+            Nome = nome;
+        }
+
+        public string Nome { get; }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Parceiro);
+        }
+
+        public bool Equals(Parceiro other)
+        {
+            return other != null &&
+                   Nome == other.Nome;
+        }
+
+        public override int GetHashCode()
+        {
+            return 285249808 + EqualityComparer<string>.Default.GetHashCode(Nome);
+        }
+
+        public override string ToString()
+        {
+            return Nome;
+        }
+    }
+}
