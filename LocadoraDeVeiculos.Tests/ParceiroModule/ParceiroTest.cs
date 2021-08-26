d965be38fc85e2091d0fbb4c4c0a33f0280ccd88
Matheus F Lopes <matheusdefigueiredo@gmail.com>
2021-08-26 16:35:34 -0300

    Testes unitários implementados.

---
+++ LocadoraDeVeiculos.Tests/ParceiroModule/ParceiroTest.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using LocadoraDeVeiculos.Dominio.ParceiroModule;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace LocadoraDeVeiculos.Tests.ParceiroModule
+{
+    [TestClass]
+    [TestCategory("Dominio")]
+    public class ParceiroTest
+    {
+        [TestMethod]
+        public void DeveCriar_Parceiro()
+        {
+            Parceiro parceiro = new Parceiro("Matheus");
+
+            parceiro.Nome.Should().Be("Matheus");
+            parceiro.Validar().Should().Be("ESTA_VALIDO");
+        }
+        
+        [TestMethod]
+        public void DeveValidae_NomeParceiro()
+        {
+            Parceiro parceiro = new Parceiro(null);
+
+            parceiro.Validar().Should().Be("O campo 'nome' não pode estar vazio.");
+        }
+    }
+}
