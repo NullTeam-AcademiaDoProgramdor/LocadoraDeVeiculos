3fc8c1b46d89d3a4c07d985e4bfb11cfefa032ed
Pedro Dircksen <dircksenpedro@gmail.com>
2021-08-27 13:42:31 -0300

    Implementado dominio do cupom

---
+++ LocadoraDeVeiculos.Dominio/CupomModule/Cupom.cs
@@ -0,0 +1,36 @@
+﻿using LocadoraDeVeiculos.Dominio.ParceiroModule;
+using LocadoraDeVeiculos.Dominio.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Dominio.CupomModule
+{
+    public class Cupom : EntidadeBase
+    {
+        public Cupom(string codigo, Parceiro parceiro, string tipo, double valor, double valorMinimo, DateTime dataVencimento)
+        {
+            Codigo = codigo;
+            Parceiro = parceiro;
+            Tipo = tipo;
+            Valor = valor;
+            ValorMinimo = valorMinimo;
+            DataVencimento = dataVencimento;
+        }
+        
+        public string Codigo { get; }
+        public Parceiro Parceiro { get; }
+        public string Tipo { get; }
+        public double Valor { get; }
+        public double ValorMinimo { get; }
+        public DateTime DataVencimento { get; }
+        public int QtdUsos { get; }
+
+        public override string Validar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
