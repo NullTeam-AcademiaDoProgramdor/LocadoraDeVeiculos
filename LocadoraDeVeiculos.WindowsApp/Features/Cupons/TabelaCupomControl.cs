8fff24ed05b20965327b426ed198a4966329aaa7
Pedro Dircksen <dircksenpedro@gmail.com>
2021-08-29 19:00:35 -0300

    Implementado o grid de visualização de cupons

---
+++ LocadoraDeVeiculos.WindowsApp/Features/Cupons/TabelaCupomControl.cs
@@ -0,0 +1,63 @@
+﻿using LocadoraDeVeiculos.Dominio.CupomModule;
+using LocadoraDeVeiculos.WindowsApp.Shared;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LocadoraDeVeiculos.WindowsApp.Features.Cupons
+{
+    public partial class TabelaCupomControl : UserControl
+    {
+        public TabelaCupomControl()
+        {
+            InitializeComponent();
+            GridCupom.ConfigurarGridZebrado();
+            GridCupom.ConfigurarGridSomenteLeitura();
+
+            GridCupom.Columns.AddRange(ObterColunas());
+        }
+
+        private DataGridViewColumn[] ObterColunas()
+        {
+            var colunas = new DataGridViewColumn[]
+            {
+                new DataGridViewTextBoxColumn { DataPropertyName = "Id", HeaderText = "Id"},
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "Codigo", HeaderText = "Código"},
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "Parceiro", HeaderText = "Parceiro"},
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "Valor", HeaderText = "Valor"},
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "Tipo", HeaderText = "Tipo" },
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "DataVencimento", HeaderText = "Data de Vencimento"},
+
+                new DataGridViewTextBoxColumn { DataPropertyName = "QtdUsos", HeaderText = "Quantidade de Usos"}
+            };
+
+            return colunas;
+        }
+
+        public int ObtemIdSelecionado()
+        {
+            return GridCupom.SelecionarId<int>();
+        }
+
+        public void AtualizarRegistros(List<Cupom> cupons)
+        {
+            CarregarTabela(cupons);
+        }
+
+        private void CarregarTabela(List<Cupom> cupons)
+        {
+            GridCupom.DataSource = cupons;
+        }
+    }
+}
