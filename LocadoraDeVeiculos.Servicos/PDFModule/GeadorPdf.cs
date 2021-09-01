a75d16bcb0fd3fe63f781922f9c2297f9c123fb2
Matheus F Lopes <matheusdefigueiredo@gmail.com>
2021-09-01 18:08:53 -0300

    Iniciada a immplementação do primeiro método (gerarPDf) no gerador de PDF.

---
+++ LocadoraDeVeiculos.Servicos/PDFModule/GeadorPdf.cs
@@ -0,0 +1,51 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using LocadoraDeVeiculos.Dominio.RelatorioModule;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LocadoraDeVeiculos.Servicos.PDFModule
+{
+    public class GeradorPdf
+    {
+        Relatorio relatorio = null;
+
+        public GeradorPdf(Relatorio relatorio)
+        {
+            this.relatorio = relatorio;
+        }
+
+        public void GerarPdf()
+        {
+            Document doc = new Document(PageSize.A4);
+            doc.SetMargins(3, 2, 3, 2);
+            doc.AddCreationDate();
+
+            string caminho = @"C:\...\...\" + "\\RelatorioLocacao.pdf";
+
+            PdfWriter writer = PdfWriter.GetInstance(doc,
+                new FileStream(caminho, FileMode.Create));
+
+            doc.Open();
+
+            PdfPTable table = new PdfPTable(3);           
+
+            Font fonte = FontFactory.GetFont(BaseFont.TIMES_ROMAN, 22);
+
+            string dados = "";
+
+            Paragraph paragrafo = new Paragraph(dados, fonte);
+
+            paragrafo.Alignment = Element.ALIGN_JUSTIFIED;
+
+            doc.Add(paragrafo);
+
+            doc.Close();
+        }
+    }
+}
