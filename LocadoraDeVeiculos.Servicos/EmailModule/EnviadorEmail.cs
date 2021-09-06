8fa3b3b58f61efde8c7a33200d181cd3d812a9d7
Erik Borela <erikborella@gmail.com>
2021-09-06 16:29:51 -0300

    Implementação do enviador de email para pegar coisas do banco de dados

--- LocadoraDeVeiculos.Servicos/EmailModule/EnviadorEmail.cs
+++ LocadoraDeVeiculos.Servicos/EmailModule/EnviadorEmail.cs
@@ -8,41 +8,41 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Net.NetworkInformation;
+using LocadoraDeVeiculos.Controladores.Shared;
+using LocadoraDeVeiculos.Controladores.RequisicaoEmailModule;
+using LocadoraDeVeiculos.Dominio.RequisicaoEmailModule;
+using System.IO;
 
 namespace LocadoraDeVeiculos.Servicos.EmailModule
 {
     public static class EnviadorEmail
     {
-        private class Email
-        {
-            public string mensagem;
-            public string emailDestino;
-            public string[] arquivos;
-
-            public Email(string mensagem, string emailDestino, string[] arquivos)
-            {
-                this.mensagem = mensagem;
-                this.emailDestino = emailDestino;
-                this.arquivos = arquivos;
-            }
-        }
-
-        private static Queue<Email> filaDeEmail;
+        private static ControladorRequisicaoEmail controlador;
+        private static Thread thread;
 
         static EnviadorEmail()
         {
-            filaDeEmail = new Queue<Email>();
+            controlador = new ControladorRequisicaoEmail();
 
-            Thread thread = new Thread(IniciarFila);
+            thread = new Thread(IniciarFila);
+        }
+
+        public static void Iniciar()
+        {
             thread.Start();
+        }
+
+        public static void Parar()
+        {
+            thread.Abort();
         }
 
         public static void AdicionarEmail(string mensagem, string emailDestino,
             params string[] pdfs)
         {
-            filaDeEmail.Enqueue(new Email(mensagem, emailDestino, pdfs));
+            controlador.InserirNovo(
+                new RequisicaoEmail(mensagem, emailDestino, pdfs));
         }
-
 
         private static void GerarClient(Action<SmtpClient> action)
         {
@@ -63,7 +63,7 @@ namespace LocadoraDeVeiculos.Servicos.EmailModule
             return @"..\..\..\Arquivos\" + arquivo;
         }
 
-        private static void Enviar(Email email)
+        private static void Enviar(RequisicaoEmail email)
         {
             using (MailMessage mail = new MailMessage())
             {
@@ -87,19 +87,23 @@ namespace LocadoraDeVeiculos.Servicos.EmailModule
 
         private static void IniciarFila()
         {
-
             while (true)
             {
-                if (!IsConnectedToInternet())
+                if (!IsConnectedToInternet() || !controlador.ExisteAlgum())
                 {
+                    Console.WriteLine("Esperando 10s para a proxima verificação de email");
                     Thread.Sleep(10 * 1000);
                     continue;
                 }
+               
+                List<RequisicaoEmail> emails = controlador.SelecionarTodos();
 
-                if (filaDeEmail.Count > 0)
-                { 
-                    Email email = filaDeEmail.Dequeue();
-                    EnviadorEmail.Enviar(email);
+                foreach (var email in emails)
+                {
+                    Console.WriteLine($"Enviando email para {email.emailDestino}");
+                    Enviar(email);
+                    controlador.Excluir(email.id);
+                    ExcluirArquivos(email.arquivos);
                 }
                 
             }
@@ -123,5 +127,13 @@ namespace LocadoraDeVeiculos.Servicos.EmailModule
             return result;
         }
 
+        private static void ExcluirArquivos(string[] arquivos)
+        {
+            foreach (var arquivo in arquivos)
+            {
+                string caminho = GetEnderecoReal(arquivo);
+                File.Delete(caminho);
+            }
+        }
     }
 }
