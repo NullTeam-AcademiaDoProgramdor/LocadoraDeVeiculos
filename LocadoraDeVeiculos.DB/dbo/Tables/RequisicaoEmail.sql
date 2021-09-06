307259ca029ebf1a811769e9749494d7e2f96334
Erik Borela <erikborella@gmail.com>
2021-09-06 15:01:49 -0300

    Criado tabela para as requisicoes de email

---
+++ LocadoraDeVeiculos.DB/dbo/Tables/RequisicaoEmail.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[RequisicaoEmail]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [mensagem] VARCHAR(1000) NULL, 
+    [emailDestino] VARCHAR(1000) NULL, 
+    [nomePdf] VARCHAR(1000) NULL
+)
