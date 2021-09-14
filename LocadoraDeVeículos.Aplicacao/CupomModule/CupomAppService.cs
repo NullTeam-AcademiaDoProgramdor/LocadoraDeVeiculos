9e11272b72f6285a85b27fc0b83c6f34c166fa95
Erik Borela <erikborella@gmail.com>
2021-09-14 14:45:30 -0300

    Camada de aplicação feita para o cupom

---
+++ LocadoraDeVeículos.Aplicacao/CupomModule/CupomAppService.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LocadoraDeVeículos.Aplicacao.Shared;
+using LocadoraDeVeiculos.Dominio.CupomModule;
+
+namespace LocadoraDeVeículos.Aplicacao.CupomModule
+{
+    public class CupomAppService : ICadastravel<Cupom>
+    {
+        private RepositorCupomBase repositorio;
+
+        public CupomAppService(RepositorCupomBase repositorio)
+        {
+            this.repositorio = repositorio;
+        }
+
+        public string Editar(int id, Cupom registro)
+        {
+            string resultadoValidacao = registro.Validar();
+
+            if (resultadoValidacao == "ESTA_VALIDO")
+            {
+                repositorio.Editar(id, registro);
+            }
+
+            return resultadoValidacao;
+        }
+
+        public bool Excluir(int id)
+        {
+            return repositorio.Excluir(id);
+        }
+
+        public bool Existe(int id)
+        {
+            return repositorio.Existe(id);
+        }
+
+        public string InserirNovo(Cupom registro)
+        {
+            string resultadoValidacao = registro.Validar();
+
+            if (resultadoValidacao == "ESTA_VALIDO")
+            {
+                repositorio.InserirNovo(registro);
+            }
+
+            return resultadoValidacao;
+        }
+
+        public Cupom SelecionarPorId(int id)
+        {
+            return repositorio.SelecionarPorId(id);
+        }
+
+        public List<Cupom> SelecionarTodos()
+        {
+            return repositorio.SelecionarTodos();
+        }
+
+        public void EditarQtdUsos(Cupom cupom)
+        {
+            repositorio.EditarQtdUsos(cupom);
+        }
+
+        public Cupom SelecionarPorCodigo(string codigo)
+        {
+            return repositorio.SelecionarPorCodigo(codigo);
+        }
+
+        public List<Cupom> SelecionarValidos()
+        {
+            return repositorio.SelecionarValidos();
+        }
+    }
+}
