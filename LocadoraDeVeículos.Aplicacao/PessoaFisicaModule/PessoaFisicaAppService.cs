5efc4a4c1a6b7477e350baf18ff937918181c780
Diconcili <eduardodiconcilicosta@yahoo.com.br>
2021-09-14 14:19:43 -0300

    PessoaFisicaAppService implementada

---
+++ LocadoraDeVeículos.Aplicacao/PessoaFisicaModule/PessoaFisicaAppService.cs
@@ -0,0 +1,65 @@
+﻿using LocadoraDeVeículos.Aplicacao.Shared;
+using LocadoraDeVeiculos.Dominio.PessoaFisicaModule;
+using LocadoraDeVeículos.Infra.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeículos.Aplicacao.PessoaFisicaModule
+{
+    class PessoaFisicaAppService : ICadastravel<PessoaFisica>
+    {
+        private IRepositor<PessoaFisica> repositorio;
+
+        public PessoaFisicaAppService(IRepositor<PessoaFisica> repositor)
+        {
+            repositorio = repositor;
+        }
+
+        public string InserirNovo(PessoaFisica registro)
+        {
+            string resultadoValidacao = registro.Validar();
+
+            if (resultadoValidacao == "ESTA_VALIDO")
+            {
+                repositorio.InserirNovo(registro);
+            }
+
+            return resultadoValidacao;
+        }
+
+        public string Editar(int id, PessoaFisica registro)
+        {
+            string resultadoValidacao = registro.Validar();
+
+            if (resultadoValidacao == "ESTA_VALIDO")
+            {
+                repositorio.Editar(id, registro);
+            }
+
+            return resultadoValidacao;
+        }
+
+        public bool Excluir(int id)
+        {
+            return repositorio.Excluir(id);
+        }
+
+        public bool Existe(int id)
+        {
+            return repositorio.Existe(id);
+        }        
+
+        public PessoaFisica SelecionarPorId(int id)
+        {
+            return repositorio.SelecionarPorId(id);
+        }
+
+        public List<PessoaFisica> SelecionarTodos()
+        {
+            return repositorio.SelecionarTodos();
+        }
+    }
+}
