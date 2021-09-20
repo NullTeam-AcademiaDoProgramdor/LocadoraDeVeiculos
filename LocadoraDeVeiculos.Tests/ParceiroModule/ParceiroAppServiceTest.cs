6b8c01f5d387ca0d573fa0e7a1366b84c338cb28
Diconcili <eduardodiconcilicosta@yahoo.com.br>
2021-09-20 16:02:24 -0300

    ParceiroAppServiceTest criada

---
+++ LocadoraDeVeiculos.Tests/ParceiroModule/ParceiroAppServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using LocadoraDeVeículos.Aplicacao.ParceiroModule;
+using LocadoraDeVeiculos.Dominio.ParceiroModule;
+using LocadoraDeVeículos.Infra.SQL.ParceiroModule;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Tests.ParceiroModule
+{
+    [TestClass]
+    public class ParceiroAppServiceTest
+    {
+        [TestMethod]
+        public void DeveInserir_NovaTaxaOuServico()
+        {
+            Mock<Parceiro> parceiroMock = new("Josue");
+
+            parceiroMock.Setup(x => x.Validar()).Returns("ESTA_VALIDO");
+
+            Mock<ParceiroDao> parceiroDaoMock = new();
+
+            ParceiroAppService parceiroAppService = new(parceiroDaoMock.Object);
+
+            parceiroAppService.InserirNovo(parceiroMock.Object);
+
+            parceiroMock.Verify(x => x.Validar());
+            parceiroDaoMock.Verify(x => x.InserirNovo(It.IsAny<Parceiro>()));
+        }
+    }
+}
