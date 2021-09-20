b08e802062c0c745a001d68901793ea9c78f18d9
Diconcili <eduardodiconcilicosta@yahoo.com.br>
2021-09-20 15:22:53 -0300

    Classe de testes para o Dao criada

---
+++ LocadoraDeVeiculos.Tests/TaxaEServicoModule/TaxasEServicosDaoTest.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using LocadoraDeVeiculos.Controladores.Shared;
+using LocadoraDeVeiculos.Dominio.TaxasEServicosModule;
+using LocadoraDeVeículos.Infra.SQL.TaxasEServicosModule;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Tests.TaxaEServicoModule
+{
+    [TestClass]
+    public class TaxasEServicosDaoTest
+    {
+        TaxasEServicosDao controlador = null;        
+
+        public TaxasEServicosDaoTest()
+        {
+            controlador = new();            
+        }
+
+        [TestCleanup()]
+        public void LimparTeste()
+        {
+            Db.Update("DELETE FROM [TaxaEServico]");            
+        }
+
+        [TestMethod]
+        public void DeveInserir_Taxa()
+        {
+            TaxaEServico novaTaxa = new("gps", 10, true);
+
+            controlador.InserirNovo(novaTaxa);
+
+            TaxaEServico taxaEncontrada = controlador.SelecionarPorId(novaTaxa.id);
+
+            taxaEncontrada.Should().Be(novaTaxa);
+        }
+
+    }
+}
