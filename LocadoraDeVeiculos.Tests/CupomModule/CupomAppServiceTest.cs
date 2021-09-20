0ee8d93a5d31c2df73d0c3a90e1c0fd29815ea2e
Pedro Dircksen <dircksenpedro@gmail.com>
2021-09-20 16:47:39 -0300

    Implementado teste para nao inserir um cupom se estiver invalido

--- LocadoraDeVeiculos.Tests/CupomModule/CupomAppServiceTest.cs
+++ LocadoraDeVeiculos.Tests/CupomModule/CupomAppServiceTest.cs
@@ -1,5 +1,7 @@
-﻿using LocadoraDeVeiculos.Dominio.CupomModule;
+﻿using LocadoraDeVeículos.Aplicacao.CupomModule;
+using LocadoraDeVeiculos.Dominio.CupomModule;
 using LocadoraDeVeiculos.Dominio.ParceiroModule;
+using LocadoraDeVeículos.Infra.SQL.CupomModule;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using System;
@@ -18,6 +20,42 @@ namespace LocadoraDeVeiculos.Tests.CupomModule
         public CupomAppServiceTest()
         {
             parceiro = gerarParceiro();
+        }
+
+        [TestMethod]
+        public void DeveInserir_Cupom()
+        {
+            Mock<Cupom> CupomMock = new("DezOff", parceiro, "Porcentagem", 10, 1000, DateTime.Today, 1);
+
+            CupomMock.Setup(x => x.Validar()).Returns("ESTA_VALIDO");
+
+            Mock<CupomDao> CupomDaoMock = new();
+
+            CupomAppService cupomAppService =
+                new(CupomDaoMock.Object);
+
+            cupomAppService.InserirNovo(CupomMock.Object);
+
+            CupomMock.Verify(x => x.Validar());
+            CupomDaoMock.Verify(x => x.InserirNovo(It.IsAny<Cupom>()));
+        }
+
+        [TestMethod]
+        public void NaoDeveInserir_Cupom()
+        {
+            Mock<Cupom> CupomMock = new("DezOff", parceiro, "Porcentagem", 10, 1000, DateTime.Today, 1);
+
+            CupomMock.Setup(x => x.Validar()).Returns("NAO_ESTA_VALIDO");
+
+            Mock<CupomDao> CupomDaoMock = new();
+
+            CupomAppService cupomAppService =
+                new(CupomDaoMock.Object);
+
+            cupomAppService.InserirNovo(CupomMock.Object);
+
+            CupomMock.Verify(x => x.Validar());
+            CupomDaoMock.Verify(x => x.InserirNovo(It.IsAny<Cupom>()), Times.Never);
         }
 
         private Parceiro gerarParceiro()
