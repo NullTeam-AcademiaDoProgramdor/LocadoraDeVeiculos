59e29315fcbf66a60784279815e9d7f5c1de3af2
Diconcili <eduardodiconcilicosta@yahoo.com.br>
2021-09-20 15:47:25 -0300

    Classe teste de domínio para Taxas e Serviços criada

---
+++ LocadoraDeVeiculos.Tests/TaxaEServicoModule/TaxaEServicoTest.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using LocadoraDeVeiculos.Dominio.TaxasEServicosModule;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Tests.TaxaEServicoModule
+{
+    [TestClass]
+    public class TaxaEServicoTest
+    {
+        [TestMethod]
+        public void DeveCriar_Taxa()
+        {
+            TaxaEServico taxa = new ("GPS", 10, true);
+
+            taxa.Nome.Should().Be("GPS");
+            taxa.Preco.Should().Be(10);
+            taxa.EhFixo.Should().Be(true);
+        }
+    }
+}
