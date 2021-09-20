038c85c5e320b9d35c4fe1e914746259d5c9e537
Pedro Dircksen <dircksenpedro@gmail.com>
2021-09-20 17:03:25 -0300

    Classe de testes da aplicação de pessoa juridica adicionada ao projeto

---
+++ LocadoraDeVeiculos.Tests/PessoaJuridicaModule/PessoaJuridicaAppServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Tests.PessoaJuridicaModule
+{
+    [TestClass]
+    public class MyTestClass
+    {
+        [TestMethod]
+        public void MyTestMethod()
+        {
+
+        }
+    }   
+}
