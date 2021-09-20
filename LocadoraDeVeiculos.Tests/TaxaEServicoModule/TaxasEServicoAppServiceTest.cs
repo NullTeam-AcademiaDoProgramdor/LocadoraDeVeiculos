949e2a85b76985cb986ef3bbec6fb65568ad412b
Diconcili <eduardodiconcilicosta@yahoo.com.br>
2021-09-20 16:07:43 -0300

    Identação corrigida

--- LocadoraDeVeiculos.Tests/TaxaEServicoModule/TaxasEServicoAppServiceTest.cs
+++ LocadoraDeVeiculos.Tests/TaxaEServicoModule/TaxasEServicoAppServiceTest.cs
@@ -64,6 +64,5 @@ namespace LocadoraDeVeiculos.Tests.TaxaEServicoModule
             taxaMock.Verify(x => x.Validar());
             taxasEServicosDaoMock.Verify(x => x.Editar(It.IsAny<int>(), It.IsAny<TaxaEServico>()));           
         }
-
     }
 }
