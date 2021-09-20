830a762ec82ec3b3cfc4bda03955f42cc6b1908d
Diconcili <eduardodiconcilicosta@yahoo.com.br>
2021-09-20 15:09:34 -0300

    Testes de validação implementados

---
+++ LocadoraDeVeiculos.Tests/TaxaEServicoModule/TaxasEServicoAppServiceTest.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LocadoraDeVeículos.Aplicacao.TaxaEServicoModule;
+using LocadoraDeVeiculos.Dominio.TaxasEServicosModule;
+using LocadoraDeVeículos.Infra.SQL.TaxasEServicosModule;
+
+namespace LocadoraDeVeiculos.Tests.TaxaEServicoModule
+{
+    [TestClass]
+    public class TaxasEServicoAppServiceTest
+    {
+        [TestMethod]
+        public void DeveInserir_NovaTaxaOuServico()
+        {
+            Mock<TaxaEServico> taxaMock = new("gps", 10, false);
+
+            taxaMock.Setup(x => x.Validar()).Returns("ESTA_VALIDO");
+
+            Mock<TaxasEServicosDao> taxasEServicosDaoMock = new();            
+
+            TaxaEServicoAppService taxasEServicosAppService = new(taxasEServicosDaoMock.Object);
+
+            taxasEServicosAppService.InserirNovo(taxaMock.Object);
+
+            taxaMock.Verify(x => x.Validar());
+            taxasEServicosDaoMock.Verify(x => x.InserirNovo(It.IsAny<TaxaEServico>()));  
+        }
+
+        [TestMethod]
+        public void NaoDeveInserir_SeNaoEstaValido()
+        {
+            Mock<TaxaEServico> taxaMock = new("gps", 10, false);
+
+            taxaMock.Setup(x => x.Validar()).Returns("NAO_ESTA_VALIDO");
+
+            Mock<TaxasEServicosDao> taxasEServicosDaoMock = new();
+
+            TaxaEServicoAppService taxasEServicosAppService = new(taxasEServicosDaoMock.Object);
+
+            taxasEServicosAppService.InserirNovo(taxaMock.Object);
+
+            taxaMock.Verify(x => x.Validar());
+            taxasEServicosDaoMock.Verify(x => x.InserirNovo(It.IsAny<TaxaEServico>()), Times.Never);            
+        }
+    }
+}
