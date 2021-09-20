560084a880cabd6c35b062876da5582ced8428ad
Diconcili <eduardodiconcilicosta@yahoo.com.br>
2021-09-20 16:20:30 -0300

    Teste de domínio do parceiro implementada

---
+++ LocadoraDeVeiculos.Tests/ParceiroModule/ParceiroTest.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using LocadoraDeVeiculos.Dominio.ParceiroModule;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Tests.ParceiroModule
+{
+    [TestClass]
+    public class ParceiroTest
+    {
+        [TestMethod]
+        public void DeveCriar_Parceiro()
+        {
+            Parceiro parceiro = new("Josue");
+
+            parceiro.Nome.Should().Be("Josue");
+        }
+
+
+    }
+}
