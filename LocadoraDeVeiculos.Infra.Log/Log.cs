ca863509e5119fc79956d8ae88a9a5f774531c9d
Pedro Dircksen <dircksenpedro@gmail.com>
2021-09-27 13:59:07 -0300

    pasta Log removida do git ignore

---
+++ LocadoraDeVeiculos.Infra.Log/Log.cs
@@ -0,0 +1,20 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace LocadoraDeVeiculos.Infra.Log
+{
+    public static class Log
+    {
+        public static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        static Log()
+        {
+            // Load configuration
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+        }
+    }
+}
