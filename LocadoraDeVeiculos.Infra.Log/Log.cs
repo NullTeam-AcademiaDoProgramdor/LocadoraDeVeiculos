442e4e1f920a5781ccc47203cb48a5724e5ed790
Erik Borela <erikborella@gmail.com>
2021-09-28 16:50:11 -0300

    Adicionada opção para poder ativar ou desativar o log detalhado

--- LocadoraDeVeiculos.Infra.Log/Log.cs
+++ LocadoraDeVeiculos.Infra.Log/Log.cs
@@ -1,5 +1,8 @@
 ﻿using Serilog;
+using Serilog.Core;
+using Serilog.Events;
 using System;
+using System.Configuration;
 using System.IO;
 using System.Reflection;
 
@@ -7,12 +10,31 @@ namespace LocadoraDeVeiculos.Infra.Log
 {
     public static class Log
     {
+
+        public static LoggingLevelSwitch levelSwitch = new();
+
         public static void ConfigurarLog()
         {
+
+            bool logDetalhado = LerSeLogEstaDetalhado();
+            SetDetalharLog(logDetalhado);
+
             Serilog.Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.ControlledBy(levelSwitch)
                 .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day)
                 .WriteTo.Seq("http://localhost:5341")
                 .CreateLogger();
         }
+        public static void SetDetalharLog(bool ativo)
+        {
+            if (ativo)
+                levelSwitch.MinimumLevel = LogEventLevel.Debug;
+            else
+                levelSwitch.MinimumLevel = LogEventLevel.Error;
+        }
+
+        private static bool LerSeLogEstaDetalhado() 
+            => Convert.ToBoolean(ConfigurationManager.AppSettings["logDetalhado"]);
+
     }
 }
