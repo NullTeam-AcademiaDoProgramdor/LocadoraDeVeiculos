8580150e41eba1e710901b0db887102a8ae3fd3a
Pedro Dircksen <dircksenpedro@gmail.com>
2021-10-07 17:31:26 -0300

    Adicionada classe ORM DAO de Locacao

---
+++ LocadoraDeVeiculos.Infra.ORM/LocacaoModule/LocacaoORMDao.cs
@@ -0,0 +1,18 @@
+﻿using LocadoraDeVeiculos.Dominio.LocacaoModule;
+using LocadoraDeVeiculos.Infra.ORM.Models;
+using LocadoraDeVeiculos.Infra.ORM.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Infra.ORM.LocacaoModule
+{
+    public class LocacaoORMDao : ORMDAOBase<Locacao>
+    {
+        public LocacaoORMDao(DBLocadoraContext db) : base(db)
+        {
+        }
+    }
+}
